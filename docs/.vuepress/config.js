1baf8414a96838b4de94369f4b98844579df1a06
Christophe Nouguier <christophe.nouguier@free.fr>
2019-01-06 00:30:41 +0100

    WIP #30

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  base: '/kdk/',
+  title: 'KDK',
+  description: 'The Kalisio Development Kit',
+  head: [
+    ['link', { rel: 'icon', href: `https://s3.eu-central-1.amazonaws.com/kalisioscope/kdk/kdk-icon-64x64.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }]
+  ],
+  serviceWorker: true,
+  theme: 'kalisio',
+  themeConfig: {
+    docsDir: 'docs',
+    serviceWorker: {
+      updatePopup: true
+    },
+    nav: [
+      {
+        text: 'What is it ?',
+        link: '/what-is-it/',
+      },
+      {
+        text: 'How doest it work ?',
+        items: [
+          
+          { text: 'Architecture', link: '/how-to-use-it/architecture' }
+        ]
+      },
+      {
+        text: 'How to use it ?',
+        items: [
+          { text: 'The Bascis', link: '/how-to-use-it/the-basics.md' },
+          { text: 'Development', link: '/how-to-use-it/development.md' },
+		  { text: 'Tools', link: '/how-to-use-it/tools.md' }
+        ]
+      },
+      {
+        text: '   ?',
+        items: [
+          { text: 'GitHub', link: 'https://github.com/kalisio/kdk' },
+          { text: 'Contributing', link: '/CONTRIBUTING.md' },
+          { text: 'License', link: '/LICENSE.md' }
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: https://s3.eu-central-1.amazonaws.com/kalisioscope/kaabah/kaabah-icon-256x256.png
+actionText: Dive into details ?
+actionLink: /what-is-it/
+footer: MIT Licensed | Copyright © 2017-2019 Kalisio
+---
+
+<center>
+  <h4>Trusted by</h4>
+  <a href="https://www.airbus.com/"><img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/assets/logos/airbus.png"></a>
+  &nbsp;
+  <a href="https://www.irsn.fr/"><img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/assets/logos/irsn.png"></a>
+  <h4>Sponsored by</h4>
+  <a href="https://kalisio.com"><img src="https://s3.eu-central-1.amazonaws.com/kalisioscope/kalisio/kalisio-logo-black-256x84.png"></a>
+</center>
