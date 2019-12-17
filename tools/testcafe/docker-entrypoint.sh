0dae10114bb0185e3b4bfa4b7fe139c5fa1ef4d3
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-17 22:15:38 +0100

    wip: Provide a default testcafe image including testcafe-vue-selectors #54

--- tools/testcafe/docker-entrypoint.sh
+++ tools/testcafe/docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -e
-
 XVFB_SCREEN_WIDTH=${SCREEN_WIDTH-1280}
 XVFB_SCREEN_HEIGHT=${SCREEN_HEIGHT-720}
 
@@ -8,4 +6,5 @@ dbus-daemon --session --fork
 Xvfb :1 -screen 0 "${XVFB_SCREEN_WIDTH}x${XVFB_SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
-node /opt/testcafe/bin/testcafe-with-v8-flag-filter.js --ports 1337,1338 "$@"
+
+node /opt/testcafe/.bin/testcafe-with-v8-flag-filter.js --ports 1337,1338 "$@"
