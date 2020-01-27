7a02f4df3d605fc24570c392d80b9801d857465f
Christophe Nouguier <christophe.nouguier@free.fr>
2020-01-28 00:24:55 +0100

    wip: Allow to run e2e on the targeted deployment platform #62

--- tools/testcafe/docker-entrypoint.sh
+++ tools/testcafe/docker-entrypoint.sh
@@ -4,6 +4,6 @@ Xvfb :1 -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 
-testcafe ${BROWSER}:headless --ports 1337,1338 --speed ${SPEED} "$@"
+testcafe ${BROWSER}:headless --ports 1337,1338 --speed ${SPEED} -S -s /screenshots -r slack "/tests/*.test.js" "$@"
 
       
