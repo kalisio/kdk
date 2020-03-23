486f8384fa7890f3a626cc40b4b722a30e15bf57
Christophe Nouguier <christophe.nouguier@free.fr>
2020-03-24 00:26:08 +0100

    feature: NavigationBar should be closable (close #72) [skip ci]

--- map/client/components/KNavigationBar.vue
+++ map/client/components/KNavigationBar.vue
@@ -1,36 +1,47 @@
 <template>
-  <div v-if="isVisible" class="row items-center k-navigation-bar no-wrap">
-    <!--
-      Before section
-    -->
-    <template v-if="(mode ==='toolbar') && hasBeforeActions">
-      <k-tool-bar :actions="navigationBar.actions.before" />
-    </template>
-    <!--
-      The location input
-    -->
-    <template v-if="hasLocationInput">
-      <q-btn v-if="(mode === 'searchbar')" icon="arrow_back" color="primary" round flat @click="mode = 'toolbar'" >
-        <q-tooltip>{{ $t('KNavigationBar.BACK') }}</q-tooltip>
-      </q-btn>
-      <k-location-input
-        :class="(mode === 'searchbar') ? 'full-width q-pr-sm' : ''"
-        :user="(mode === 'toolbar')"
-        :map="null"
-        :search="(mode === 'searchbar')"
-        :dense="true"
-        :style=""
-        @input="onLocationChanged" />
-      <q-btn v-if="(mode === 'toolbar')" icon="search" color="primary" round flat @click="mode = 'searchbar'" >
-        <q-tooltip>{{ $t('KNavigationBar.SEARCH') }}</q-tooltip>
-      </q-btn>
-    </template>
-    <!--
-      After section
-    -->
-    <template v-if="(mode === 'toolbar') && hasAfterActions">
-      <k-tool-bar :actions="navigationBar.actions.after" />
-    </template>
+  <div>
+    <div class="row items-center k-navigation-bar no-wrap">
+      <transition
+        appear
+        enter-active-class="animated fadeInRight"
+        leave-active-class="animated fadeOutRight"> 
+        <div v-if="!isFolded" class="row items-center no-wrap">
+          <!--
+            Before section
+            -->
+          <template v-if="(mode ==='toolbar') && hasBeforeActions">
+            <k-tool-bar :actions="navigationBar.actions.before" />
+          </template>
+          <!--
+            The location input
+            -->
+          <template v-if="hasLocationInput">
+            <q-btn v-if="(mode === 'searchbar')" icon="arrow_back" color="primary" round flat @click="mode = 'toolbar'" >
+              <q-tooltip>{{ $t('KNavigationBar.BACK') }}</q-tooltip>
+            </q-btn>
+            <k-location-input
+              :class="(mode === 'searchbar') ? 'full-width q-pr-sm' : ''"
+              :user="(mode === 'toolbar')"
+              :map="null"
+              :search="(mode === 'searchbar')"
+              :dense="true"
+              style=""
+              @input="onLocationChanged" />
+            <q-btn v-if="(mode === 'toolbar')" icon="search" color="primary" round flat @click="mode = 'searchbar'" >
+              <q-tooltip>{{ $t('KNavigationBar.SEARCH') }}</q-tooltip>
+            </q-btn>
+          </template>
+          <!--
+            After section
+            -->
+          <template v-if="(mode === 'toolbar') && hasAfterActions">
+            <k-tool-bar :actions="navigationBar.actions.after" />
+          </template>
+          <q-separator vertical />
+        </div>
+      </transition>
+      <q-btn flat color="primary" :size="isFolded ? 'lg' : 'md'" :icon="isFolded ? 'explore' : 'close'" @click="isFolded=!isFolded" />
+    </div>
   </div>
 </template>
 
@@ -40,19 +51,30 @@ import _ from 'lodash'
 export default {
   name: 'k-navigation-bar',
   inject: ['kActivity'],
-  data () {
-    return {
-      navigationBar: this.$store.get('navigationBar'),
-      mode: 'toolbar'
-    }
-  },
   computed: {
     hasBeforeActions () { return _.get(this.navigationBar, 'actions.before', []).length > 0 },
     hasAfterActions () { return _.get(this.navigationBar, 'actions.after', []).length > 0 },
     hasLocationInput () { return _.get(this.navigationBar, 'locationInput') },
     isVisible () { return this.hasBeforeActions || this.hasAfterActions || this.hasLocationInput }
   },
+  data () {
+    return {
+      isFolded: false,
+      isNavigatioBarVisible: true,
+      toggleNavigationBarIcon: 'keyboard_arrow_right',
+      navigationBar: this.$store.get('navigationBar'),
+      mode: 'toolbar'
+    }
+  },
   methods: {
+    toggleNavigationBar () {
+      this.isNavigatioBarVisible = !this.isNavigatioBarVisible
+      if (this.isNavigatioBarVisible) this.toggleNavigationBarIcon='close'
+      else  this.toggleNavigationBarIcon='keyboard_arrow_right'
+    },
+    toggleSideNav () {
+      //this.isNavigatioBarVisible = false
+    },
     onLocationChanged (location) {
       if (location) this.kActivity.center(location.longitude, location.latitude)
     }
