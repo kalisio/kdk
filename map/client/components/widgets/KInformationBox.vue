6e79b90279c01e2ac3665ff9842344725b445227
Luc Claustres <luc.claustres@orange.fr>
2020-05-04 19:13:13 +0200

    Fixed missing component

---
+++ map/client/components/widgets/KInformationBox.vue
@@ -0,0 +1,135 @@
+<template>
+  <div :style="widgetStyle()">
+    <div v-if="schema" class="fit row">
+      <k-tool-bar class="q-pa-sm" :actions="actions" direction="vertical" dense />
+      <q-scroll-area class="col fit" :thumb-style="thumbStyle" :bar-style="barStyle">
+        <k-view class="q-pa-md" :schema="schema" :values="properties" :options="viewOptions" />
+      </q-scroll-area>
+    </div>
+    <div v-else class="absolute-center">
+      <k-label :text="$t('KInformationBox.NO_FEATURE_SELECTED')" icon-size="48px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import centroid from '@turf/centroid'
+import { colors, copyToClipboard, exportFile  } from 'quasar'
+import { baseWidget } from '../../../../core/client/mixins'
+import { generatePropertiesSchema } from '../../utils'
+
+export default {
+  name: 'k-information-box',
+  inject: ['kActivity'],
+  mixins: [baseWidget],
+  props: {
+    feature: {
+      type: Object,
+      default: null
+    },
+    layer: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      thumbStyle: {
+        right: '4px',
+        borderRadius: '5px',
+        backgroundColor: colors.getBrand('secondary'),
+        width: '5px',
+        opacity: 0.75
+      },
+      barStyle: {
+        right: '2px',
+        borderRadius: '9px',
+        backgroundColor: colors.getBrand('primary'),
+        width: '9px',
+        opacity: 0.25
+      },
+      viewOptions: {
+        labelWidth: 5,
+        separators: true
+      },
+      schema: null,
+      properties: null,
+      actions: []
+    }
+  },
+  watch: {
+    feature: function () {
+      this.refresh()
+    }
+  },
+  methods: {
+    refresh () {
+      this.properties = null
+      this.schema = null
+      if (this.feature && this.layer) {
+        // Is there any schema ?
+        if (this.layer.schema) {
+          if (this.schema !== this.layer.schema.content) this.schema = this.layer.schema.content
+        } else {
+          this.schema = generatePropertiesSchema(this.feature)
+        }
+        // Ensure schema is not empty
+        if (Object.keys(this.schema.properties).length === 0) {
+          return
+        }
+        // Retirve properties on feature/entity
+        if (this.kActivity.is2D()) {
+          this.properties = this.kActivity.generateLeafletStyle('infobox', this.feature, this.layer)
+        } else {
+          this.properties = this.kActivity.generateCesiumStyle('infobox', this.feature, this.layer)
+        }
+        this.kActivity.centerOnSelection()
+        this.kActivity.addSelectionHighlight('information-box')
+      }
+    },
+    onCenterOn () {
+      this.kActivity.centerOnSelection()
+    },
+    async onCopyProperties () {
+      if (this.feature) {
+        try {
+          await copyToClipboard(JSON.stringify(this.feature.properties))
+          this.$toast({ type: 'positive', message: this.$t('KInformationBox.PROPERTIES_COPIED') })
+        } catch (_) {
+          this.$toast({ type: 'error', message: this.$t('KInformationBox.CANNOT_COPY_PROPERTIES') })
+        }
+      }
+    },
+    onExportFeature () {
+      if (this.feature) {
+        const name = _.get(this.feature, 'name', 'feature') || 
+                     _.get(this.feature, 'label', 'feature') || 
+                     _.get(this.feature, 'properties.name', 'feature') || 
+                     _.get(this.feature, 'properties.label', 'feature')
+        const file = name + '.geojson'
+        const status = exportFile(file, JSON.stringify(this.feature))
+        if (status) this.$toast({ type: 'error', message: this.$t('KInformationBox.FEATURE_EXPORTED', { file }) })
+        else this.$toast({ type: 'error', message: this.$t('KInformationBox.CANNOT_EXPORT_FEATURE') })
+      }
+    }
+  },
+  created () {
+    // laod the required components
+    this.$options.components['k-tool-bar'] = this.$load('layout/KToolBar')
+    this.$options.components['k-view'] = this.$load('form/KView')
+    this.$options.components['k-label'] = this.$load('frame/KLabel')
+    // Registers the actions
+    this.actions = [
+      { name: 'centerOn', icon: 'las la-eye', label: this.$t('KInformationBox.CENTER_ON'), handler: this.onCenterOn },
+      { name: 'copyProperties', icon: 'las la-clipboard', label: this.$t('KInformationBox.COPY_PROPERTIES'), handler: this.onCopyProperties }, 
+      { name: 'exportFeature', icon: 'img:statics/json-icon.svg', label: this.$t('KInformationBox.EXPORT_FEATURE'), handler: this.onExportFeature }, 
+    ]
+    // Refresh the component
+    this.refresh()
+  },
+  beforeDestroy() {
+    this.kActivity.removeSelectionHighlight('information-box')
+  }
+}
+</script>
