9fc1153f7304eab19af10bade926b96d1b87dac2
Luc Claustres <luc.claustres@orange.fr>
2020-06-08 19:12:15 +0200

    feat: Allow emit hook to skip event emission closes https://github.com/kalisio/kdk/issues/179

--- core/client/hooks/hooks.events.js
+++ core/client/hooks/hooks.events.js
@@ -1,5 +1,7 @@
+import _ from 'lodash'
 import { Events } from '../events'
 
 export function emit (hook) {
+  if (_.get(hook, `params.skip-${hook.type}-event`)) return
   Events.$emit(hook.type + '-hook', hook)
 }
--- core/client/hooks/hooks.users.js
+++ core/client/hooks/hooks.users.js
@@ -1,16 +1,22 @@
 export async function checkUnique (hook) {
   const accountService = hook.app.getService('account')
   try {
-	  await accountService.create({
-	    action: 'checkUnique',
-	    value: { email: hook.data.email }
-	  })
-	} catch (error) {
-		// BadRequest is thrown when the user already exists, use a more specific message in this case
-		if (error.code === 400) {
-			error.data.translation = { key: 'EMAIL_ALREADY_TAKEN' }
-		}
-		throw error
-	}
+    await accountService.create({
+      action: 'checkUnique',
+      value: { email: hook.data.email }
+    }, {
+      // As we manage error we make this call transparent from the client perspective
+      // This will avoid displaying an error message twice
+      'skip-before-event': true,
+      'skip-after-event': true,
+      'skip-error-event': true
+    })
+  } catch (error) {
+    // BadRequest is thrown when the user already exists, use a more specific message in this case
+    if (error.code === 400) {
+      error.data.translation = { key: 'EMAIL_ALREADY_TAKEN' }
+    }
+    throw error
+  }
   return hook
 }
