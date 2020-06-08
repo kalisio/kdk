75b1d263d9f16c981a6c2df68d0af6a156cf665c
Luc Claustres <luc.claustres@orange.fr>
2020-06-08 14:56:43 +0200

    Fixed https://github.com/kalisio/kdk/issues/175

--- core/client/hooks/hooks.users.js
+++ core/client/hooks/hooks.users.js
@@ -1,8 +1,16 @@
 export async function checkUnique (hook) {
   const accountService = hook.app.getService('account')
-  await accountService.create({
-    action: 'checkUnique',
-    value: { email: hook.data.email }
-  })
+  try {
+	  await accountService.create({
+	    action: 'checkUnique',
+	    value: { email: hook.data.email }
+	  })
+	} catch (error) {
+		// BadRequest is thrown when the user already exists, use a more specific message in this case
+		if (error.code === 400) {
+			error.data.translation = { key: 'EMAIL_ALREADY_TAKEN' }
+		}
+		throw error
+	}
   return hook
 }
