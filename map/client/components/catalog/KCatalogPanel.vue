3a5c126ce0f327cf6a303714beedec2cb54c5d94
Robin Bourianes <robin.bourianes@frizko.com>
2020-06-11 12:46:07 +0200

    Revert "feat: add ids to key components (useful for lookup with testcafe)"
    This reverts commit e1e833103b4f1f5d120009e4308b2bb519060340.

--- map/client/components/catalog/KCatalogPanel.vue
+++ map/client/components/catalog/KCatalogPanel.vue
@@ -4,7 +4,6 @@
     <template v-for="category in layerCategories">
       <q-expansion-item
         v-if="layersByCategory[category.name].length > 0"
-        :id="category.name"
         :key="category.name"
         header-class="text-primary"
         :icon="category.icon"
