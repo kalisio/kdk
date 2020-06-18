a20dd0ba2c2c5c6e9dd22eeb70ebbda5c23c9515
Robin Bourianes <robin.bourianes@frizko.com>
2020-06-18 12:14:13 +0200

    Use slack and default reporters for testcafe.

--- extras/testcafe/docker-entrypoint.sh
+++ extras/testcafe/docker-entrypoint.sh
@@ -4,7 +4,7 @@ Xvfb :1 -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 
-testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/screenshots,takeOnFails=true -r slack "/tests/*.test.js" "$@"
+testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/screenshots,takeOnFails=true -r spec,slack "/tests/*.test.js" "$@"
 RESULT=$?
 
 if [ -n $S3_ARTEFACTS_PATH ]; then
