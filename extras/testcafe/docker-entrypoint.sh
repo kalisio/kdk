fad1424a1c53e53e08b4d9b0f50ef37f1b744e49
Robin Bourianes <robin.bourianes@frizko.com>
2020-06-18 12:05:01 +0200

    Exit test script with tests exit code.

--- extras/testcafe/docker-entrypoint.sh
+++ extras/testcafe/docker-entrypoint.sh
@@ -4,10 +4,13 @@ Xvfb :1 -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 
-testcafe ${BROWSERS} --ports 1337,1338 --speed ${SPEED} -s path=/screenshots,takeOnFails=true -r slack "/tests/*.test.js" "$@"
+testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/screenshots,takeOnFails=true -r slack "/tests/*.test.js" "$@"
+RESULT=$?
 
 if [ -n $S3_ARTEFACTS_PATH ]; then
     cd /tmp
     tar cf screenshots.tar /screenshots
     aws s3 cp screenshots.tar $S3_ARTEFACTS_PATH
 fi
+
+exit $RESULT
