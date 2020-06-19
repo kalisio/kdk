203a0ddf2d814dda498dacf840c8f6dfeb143257
Robin Bourianes <robin.bourianes@frizko.com>
2020-06-19 16:10:05 +0200

    fix: set screenshot path in /tmp.

--- extras/testcafe/docker-entrypoint.sh
+++ extras/testcafe/docker-entrypoint.sh
@@ -5,13 +5,13 @@ Xvfb :1 -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 
-testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/screenshots,takeOnFails=true -r spec,slack "/tests/*.test.js" "$@"
+testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/tmp/screenshots,takeOnFails=true -r spec,slack "/tests/*.test.js" "$@"
 RESULT=$?
 
 # Upload screenshots somewhere ?
 if [ -n "$SCREENSHOTS_RCLONE_PATH" ]; then
     cd /tmp
-    tar cf screenshots.tar /screenshots
+    tar cf screenshots.tar /tmp/screenshots
     rclone copy screenshots.tar $SCREENSHOTS_RCLONE_PATH
 fi
 
