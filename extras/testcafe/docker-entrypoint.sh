c19a1284a9fcbbf2d26590a8731eb11e61596f39
Robin Bourianes <robin.bourianes@frizko.com>
2020-06-22 13:46:28 +0200

    chore: use BUILD_BUCKET.

--- extras/testcafe/docker-entrypoint.sh
+++ extras/testcafe/docker-entrypoint.sh
@@ -5,16 +5,22 @@ Xvfb :1 -screen 0 "${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
 
-testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/tmp/screenshots,takeOnFails=true -r spec,slack "/tests/*.test.js" "$@"
+testcafe ${BROWSERS} --ports 1337,1338 --skip-js-errors --speed ${SPEED} -s path=/tmp/screenshots,takeOnFails=true -r spec,slack "/tests/*.test.js" "$@" 2>&1 > /tmp/testcafe.log
 RESULT=$?
 
-# Upload screenshots somewhere ?
-if [ -n "$SCREENSHOTS_RCLONE_PATH" ]; then
+# Upload artefacts somewhere ?
+if [ -n "$BUILD_BUCKET" ]; then
+    # testcafe test log
+    rclone copy /tmp/testcafe.log $BUILD_BUCKET
+
+    # screenshots
     cd /tmp
     tar cf screenshots.tar /tmp/screenshots
-    rclone copy screenshots.tar $SCREENSHOTS_RCLONE_PATH
+    rclone copy screenshots.tar $BUILD_BUCKET
+
+    # notify on slack
     if [ -n "$TESTCAFE_SLACK_WEBHOOK" ]; then
-        PAYLOAD=$(printf '{"text":"Test screenshots are here: %s"}' $SCREENSHOTS_RCLONE_PATH)
+        PAYLOAD=$(printf '{"text":"Test artefacts uploaded here: %s"}' $BUILD_BUCKET)
         curl -X POST -H 'Content-type: application/json' --data "$PAYLOAD" $TESTCAFE_SLACK_WEBHOOK
     fi
 fi
