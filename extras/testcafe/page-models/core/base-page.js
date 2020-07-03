ed64329957c03af9e69603fb1a575ded44fa6858
Robin Bourianes <robin.bourianes@frizko.com>
2020-07-03 12:11:15 +0200

    test: these don't exist anymore.

--- extras/testcafe/page-models/core/base-page.js
+++ extras/testcafe/page-models/core/base-page.js
@@ -3,7 +3,6 @@ import _ from 'lodash'
 import { Selector } from 'testcafe'
 import png from 'pngjs'
 import pixelmatch from 'pixelmatch'
-// import VueSelector from 'testcafe-vue-selectors'
 
 /* Given a screenshot key (a string) this will provide the full path
  * of the associated reference screenshot file.
@@ -54,23 +53,6 @@ export default class BasePage {
   // Error helper
   async isErrorVisible () {
     return this.error.visible
-  }
-
-  // Colection helpers
-  async getItem (test, collectionSelector, name) {
-    const collection = await collectionSelector.getVue()
-    return _.find(collection.state.items, { name: name })
-  }
-
-  async getItemId (test, collectionSelector, name) {
-    const item = await this.getItem(test, collectionSelector, name)
-    if (item) return item._id
-    return undefined
-  }
-
-  async checkCollectionCount (test, collectionSelector, count) {
-    const collection = await collectionSelector.getVue()
-    await test.expect(collection.state.items.length).eql(count, 'Invalid collection length')
   }
 
   // Screenshot helpers
