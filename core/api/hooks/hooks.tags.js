a2b62514329d2cd96106fec6be372882e936e63c
Luc Claustres <luc.claustres@orange.fr>
2020-07-10 13:12:58 +0200

    Fixed https://github.com/kalisio/kdk/issues/210

--- core/api/hooks/hooks.tags.js
+++ core/api/hooks/hooks.tags.js
@@ -7,6 +7,9 @@ import makeDebug from 'debug'
 const debug = makeDebug('kdk:core:tags:hooks')
 
 export function isTagEqual (tag1, tag2) {
+  // If tags already exists in DB directly use IDs for comparison
+  if (tag1._id && tag2._id) return (tag1._id.toString() === tag2._id.toString())
+
   let equal = (tag1.value === tag2.value && tag1.scope === tag2.scope)
   // when context is provided use it as well
   if (tag1.context && tag2.context) {
