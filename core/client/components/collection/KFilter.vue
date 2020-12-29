c6f6435cb44192cdd701588e48483a8a2f6ea577
Christophe Nouguier <christophe.nouguier@free.fr>
2020-12-29 12:07:34 +0100

    feat: Provide a filter component to use for filtering collections (close #283)

---
+++ core/client/components/collection/KFilter.vue
@@ -0,0 +1,59 @@
+<template>
+  <k-item-chooser
+    :multiselect="true"
+    :services="services"
+    :default-items="items"
+    @changed="onItemsChanged" />
+</template>
+
+<script>
+import { Events } from '../../events'
+
+export default {
+  name: 'k-search',
+  props: {
+    field: {
+      type: String,
+      default: 'name'
+    },
+    services: {
+      type: Array,
+      default: () => []
+    },
+    items: {
+      type: Array,
+      default: () => []
+    },
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+    
+    }
+  },
+  methods: {
+    onItemsChanged (items, pattern) {
+      const query = {}
+      // Handle the pattern
+      if (pattern !== '') {
+        query[this.field] = { $search: pattern }
+      }
+      // Handle the selection
+      items.forEach(item => {
+        // We must have only one item per service
+        const queryPath = item.service + '.' + item.field
+        query[queryPath] = item[item.field]
+      })
+      Events.$emit('filter-changed', query)
+    }
+  },
+  created () {
+    // Load the required components
+    this.$options.components['k-item-chooser'] = this.$load('input/KItemChooser')
+  }
+}
+</script>
+
