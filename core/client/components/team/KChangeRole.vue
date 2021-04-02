29678c3f8fc836605f3f05f59432f04817dd8971
Christophe Nouguier <christophe.nouguier@free.fr>
2021-04-02 16:17:02 +0200

    fix: Default role not set when editing member role in group (close #229)

--- core/client/components/team/KChangeRole.vue
+++ core/client/components/team/KChangeRole.vue
@@ -1,24 +1,26 @@
 <template>
   <k-modal
     id="change-role-modal"
-    :title="$t('KChangeRole.TITLE')" 
+    :title="title"
     :buttons="getButtons()" 
     v-model="isModalOpened"
     @opened="$emit('opened')"
     @closed="$emit('closed')">
     <div slot="modal-content" class="column xs-gutter">
-      <k-form ref="form" :schema="getSchema()" />
+      <k-form v-if="member" ref="form" :schema="getSchema()" />
     </div>
   </k-modal>
 </template>
 
 <script>
+import _ from 'lodash'
 import mixins from '../../mixins'
 
 export default {
   name: 'k-change-role',
   mixins: [
     mixins.baseModal,
+    mixins.objectProxy,
     mixins.refsResolver(['form'])
   ],
   props: {
@@ -35,26 +37,37 @@ export default {
       required: true
     }
   },
+  computed: {
+    title () {
+      return this.$t('KChangeRole.TITLE', { member: this.member ? this.member.name : '' })
+    }
+  },
+  data () {
+    return {
+      member: null
+    }
+  },
   methods: {
     getSchema () {
+      const currentRole = _.get(_.find(this.member.organisations, { _id: this.contextId }), 'permissions')
+      const roles = [
+        { label: this.$t('KChangeRole.MEMBER_LABEL'), value: 'member' },
+        { label: this.$t('KChangeRole.MANAGER_LABEL'), value: 'manager' },
+        { label: this.$t('KChangeRole.OWNER_LABEL'), value: 'owner' }
+      ]
+      _.remove(roles, role => { return role.value === currentRole })
       return {
         $schema: 'http://json-schema.org/draft-06/schema#',
         $id: 'http://kalisio.xyz/schemas/change-role#',
-        title: 'Change role form',
         type: 'object',
         properties: {
           role: {
             type: 'string',
-            default: 'member',
             field: {
               component: 'form/KSelectField',
               label: 'KChangeRole.ROLE_FIELD_LABEL',
               type: 'radio',
-              options: [
-                { label: this.$t('KChangeRole.MEMBER_LABEL'), value: 'member' },
-                { label: this.$t('KChangeRole.MANAGER_LABEL'), value: 'manager' },
-                { label: this.$t('KChangeRole.OWNER_LABEL'), value: 'owner' }
-              ]
+              options: roles
             }
           }
         },
@@ -62,10 +75,16 @@ export default {
       }
     },
     getButtons () {
-      return [
-        { id: 'update-button', label: 'KChangeRole.UPDATE_BUTTON', color: 'primary', handler: () => this.doUpdate() }
-      ]
+      return [{ 
+        id: 'update-button', 
+        label: 'KChangeRole.UPDATE_BUTTON', 
+        renderer: 'form-button', 
+        handler: () => this.doUpdate() 
+      }]
     },
+    loadService () {
+      return this.$api.getService('members')
+    },    
     async doUpdate () {
       const result = this.$refs.form.validate()
       if (result.isValid) {
@@ -82,10 +101,12 @@ export default {
       }
     }
   },
-  created () {
+  async created () {
     // Load the required components
     this.$options.components['k-modal'] = this.$load('frame/KModal')
     this.$options.components['k-form'] = this.$load('form/KForm')
+    // Load the member
+    this.member = await this.loadObject()
   }
 }
 </script>
