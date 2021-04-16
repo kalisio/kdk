b5a583f1e8f4f57a5d958e9c14ff62441072c6fc
Christophe Nouguier <christophe.nouguier@free.fr>
2021-04-16 15:57:35 +0200

    wip; Allow to apply a theme #341

--- core/client/theme.js
+++ core/client/theme.js
@@ -1,13 +1,17 @@
+import _ from 'lodash'
 import { colors } from 'quasar'
 
 export const Theme = {
   initialize () {
     this.brandPrimary = colors.getBrand('primary')
   },
-  apply (primary) {
-    colors.setBrand('primary', primary)
-    colors.setBrand('secondary', colors.lighten(primary, 25))
-    colors.setBrand('accent', colors.lighten(primary, 75))
+  apply (color) {
+    if (_.isEmpty(color)) this.restore()
+    else {
+      colors.setBrand('primary', color)
+      colors.setBrand('secondary', colors.lighten(color, 25))
+      colors.setBrand('accent', colors.lighten(color, 75))
+    }
   },
   restore () {
     this.apply(this.brandPrimary)
