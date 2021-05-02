5ffde079cb8def05827126a856ff51d62813c953
cnouguier <christophe.nouguier@free.fr>
2021-05-02 22:53:54 +0200

    chore: homogenize cards

--- core/client/components/team/KTagCard.vue
+++ core/client/components/team/KTagCard.vue
@@ -2,8 +2,29 @@
   <k-card v-bind="$props" :actions="itemActions" :bind-actions="false" :options="{ nameField: 'value' }">
     <div slot="card-label">
       <span class="text-subtitle1 text-weight-medium ellipsis">{{ item.value }}</span>
-      <q-btn class="float-right" :key="item._id" flat small rounded color="primary"
-          id="tag-count" :icon="roleIcons[0]" :label="item.count"/>
+    </div>
+    <!--
+      Card content
+     -->
+    <div slot="card-content">
+      <q-list bordered>
+        <q-item 
+          :id="item._id" 
+          clickable 
+          @click="onListMembers">
+          <q-item-section avatar>
+            <q-icon name="las la-user-friends" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              {{ $t(`KTagCard.MEMBERS_LABEL`, { count: item.count }) }}
+            </q-item-label>
+            <q-tooltip>
+              {{ $t(`KTagCard.VIEW_MEMBERS_LABEL`) }}
+            </q-tooltip>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </div>
   </k-card>
 </template>
