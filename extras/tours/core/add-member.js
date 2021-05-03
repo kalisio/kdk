2f20d1f8dc34e899c430d0e34b7fcb3ef660bbfd
cnouguier <christophe.nouguier@free.fr>
2021-05-03 21:48:18 +0200

    chore: updated add-member tour

--- extras/tours/core/add-member.js
+++ extras/tours/core/add-member.js
@@ -6,14 +6,43 @@ module.exports = [{
   }
 }, {
   target: '#email-field',
-  content: 'tours.add-member.UNQOUE_EMAIL_LABEL',
+  content: 'tours.add-member.UNIQUE_EMAIL_LABEL',
   params: {
     placement: 'left',
     clickDelay: 500,
-    clickOnNext: '#multiple-tab',
+    typeTextOnNext: '#email-field',
+    textOnNext: 'guest@organisation.com',
     nextDelay: 500,
     clickOnPrevious: '#unique-tab',
     previousDelay: 500
+  }
+}, {
+  target: '#continue-button',
+  content: 'tours.add-member.UNIQUE_CONTINUE_BUTTON_LABEL',
+  params: {
+    placement: 'left',
+    clickOnNext: '#continue-button',
+    nextDelay: 500
+  }
+}, {
+  target: '#name-field',
+  content: 'tours.add-member.UNIQUE_NAME_LABEL',
+  params: {
+    placement: 'left'
+  }
+}, {
+  target: '#role-field',
+  content: 'tours.add-member.UNIQUE_ROLE_LABEL',
+  params: {
+    placement: 'left',
+  }
+}, {
+  target: '#add-button',
+  content: 'tours.add-member.UNIQUE_ADD_BUTTON_LABEL',
+  params: {
+    placement: 'left',
+    nextDelay: 500,
+    clickOnNext: '#multiple-tab'
   }
 }, {
   target: '#multiple-tab',
@@ -29,7 +58,7 @@ module.exports = [{
   }
 }, {
   target: '#add-button',
-  content: 'tours.add-member.MULTIPLE_BUTTON_LABEL',
+  content: 'tours.add-member.MULTIPLE_ADD_BUTTON_LABEL',
   params: {
     placement: 'left'
   }
