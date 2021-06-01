9f3a004b2ce4ef2b39f465fb656c54f19037bf00
Robin Bourianes <robin.bourianes@kalisio.com>
2021-06-01 11:28:18 +0200

    fix: scan loaded layers when creating the KUrlLegend component (close #364)

--- map/client/components/KUrlLegend.vue
+++ map/client/components/KUrlLegend.vue
@@ -18,7 +18,7 @@ export default {
     }
   },
   methods: {
-    async onUrlLegendShowLayer (layer, engineLayer) {
+    async tryAddLegend (layer) {
       let legendUrl = _.get(layer, 'legendUrl')
       if (!legendUrl) {
         // no legend on layer, try to fetch legend in case of WMS layer
@@ -59,23 +59,35 @@ export default {
         }
       }
     },
-    onUrlLegendHideLayer (layer) {
+    tryRemoveLegend (layer) {
       const legendUrl = this.urlLegendLayers[layer._id]
       if (legendUrl) {
         const index = this.legendUrls.indexOf(legendUrl)
         this.legendUrls.splice(index, 1)
       }
+    },
+    onShowLayer (layer, engineLayer) {
+      this.tryAddLegend(layer)
+    },
+    onHideLayer (layer) {
+      this.tryRemoveLegend(layer)
     }
   },
   mounted () {
     this.urlLegendLayers = {}
-    this.kActivity.$on('layer-shown', this.onUrlLegendShowLayer)
-    this.kActivity.$on('layer-hidden', this.onUrlLegendHideLayer)
+    this.kActivity.$on('layer-shown', this.onShowLayer)
+    this.kActivity.$on('layer-hidden', this.onHideLayer)
+
+    // initial scan of already added layers
+    this.kActivity.getLayers().forEach((layer) => {
+      if (_.get(layer, 'leaflet.isVisible', false)) {
+        this.tryAddLegend(layer)
+      }
+    })
   },
   beforeDestroy () {
-    // Delete reference to the legend layer
-    this.kActivity.$off('layer-shown', this.onUrlLegendShowLayer)
-    this.kActivity.$off('layer-hidden', this.onUrlLegendHideLayer)
+    this.kActivity.$off('layer-shown', this.onShowLayer)
+    this.kActivity.$off('layer-hidden', this.onHideLayer)
   }
 }
 </script>
