7dc0b45bd366945d4e3818b8e1726242a2fcd846
cnouguier <christophe.nouguier@free.fr>
2021-06-10 15:50:45 +0200

    style: removed unused code

--- map/client/mixins/map/mixin.edit-layers.js
+++ map/client/mixins/map/mixin.edit-layers.js
@@ -6,14 +6,6 @@ import i18next from 'i18next'
 import { Dialog, uid } from 'quasar'
 import { bindLeafletEvents, unbindLeafletEvents } from '../../utils'
 
-// Please refer to https://github.com/Leaflet/Leaflet.draw/issues/695
-const originalOnTouch = L.Draw.Polyline.prototype._onTouch
-L.Draw.Polyline.prototype._onTouch = function (e) {
-  if (e.originalEvent.pointerType !== 'mouse') {
-    return originalOnTouch.call(this, e)
-  }
-}
-
 export default {
   methods: {
     isLayerEdited (layer) {
