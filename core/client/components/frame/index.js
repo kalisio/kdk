14d9382215e7f2eb02ec079f2f38826a4d958287
cnouguier <christophe.nouguier@free.fr>
2021-06-23 21:14:29 +0200

    chore: updated frame/index.js

--- core/client/components/frame/index.js
+++ core/client/components/frame/index.js
@@ -1,19 +1,33 @@
 import KAction from './KAction.vue'
 import KAvatar from './KAvatar.vue'
 import KBlock from './KBlock.vue'
+import KChart from './KChart.vue'
+import KChipsPane from './KChipsPane.vue'
 import KContent from './KContent.vue'
 import KModal from './KModal.vue'
+import KOpener from './KOpener.vue'
+import KPageSticky from './KPageSticky.vue'
 import KPanel from './KPanel.vue'
+import KPopupActions from './KPopupAction.vue'
 import KScreen from './KScreen.vue'
+import KSpot from './KSpot.vue'
+import KStamp from './KStamp.vue'
 import KTextArea from './KTextArea.vue'
 
 export {
   KAction,
   KAvatar,
   KBlock,
+  KChart,
+  KChipsPane,
   KContent,
   KModal,
-  KPanel,
+  KOpener,
+  KPageSticky,
+  KPanel,  
+  KPopupAction,
   KScreen,
+  KSpot,
+  KStamp,
   KTextArea
 }
