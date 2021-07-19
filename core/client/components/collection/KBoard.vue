f0d6206545fcc9359d1f85fc4c705e7b01fb633b
cnouguier <christophe.nouguier@free.fr>
2021-07-19 19:37:03 +0200

    chore: fix board alignment

--- core/client/components/collection/KBoard.vue
+++ core/client/components/collection/KBoard.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="fit row justify-center q-gutter-x-sm no-wrap">
+  <div class="fit row justify-between q-gutter-x-sm no-wrap">
     <template v-for="column in columns">
       <div
         :id="column.value"
         :key="column.value"
-        :style="{ minWidth: `${column.width}px` }">
-          <k-column
-            :label="column.label"
-            :key="column.value" 
-            v-bind="column.props" 
-            :append-items="true" 
-            :height="height" />
+        :style="{ minWidth: `${column.width}px` }"
+      >
+        <k-column
+          :label="column.label"
+          :key="column.value" 
+          v-bind="column.props" 
+          :append-items="true" 
+          :height="height" />
       </div>
     </template>
   </div>
@@ -21,7 +22,7 @@ import KColumn from './KColumn.vue'
 
 export default {
   name: 'k-board',
-   components: { 
+  components: { 
     KColumn
   },
   props: {
