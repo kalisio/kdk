92c5954987b1d07c00762e16fc728f09d7ed1d0a
cnouguier <christophe.nouguier@free.fr>
2021-08-27 11:34:09 +0200

    fix: KStamp does not handle the textSize prop (close #456)

--- core/client/components/frame/KStamp.vue
+++ core/client/components/frame/KStamp.vue
@@ -9,11 +9,11 @@
       <div style="text-align: center">
         <q-icon v-if="showIcon" :size="iconSize" :name="icon" />
           <q-tooltip v-if="!showText">
-            {{ $t(text) }}
+            {{ translatedText }}
           </q-tooltip>
       </div>
-      <div v-if="showText" class="q-pl-xs q-pr-xs ellipsis-2-lines">
-         {{ $t(text) }}
+      <div v-if="showText" class="q-pl-xs q-pr-xs ellipsis-2-lines" :style="`font-size: ${textSize}`">
+         {{ translatedText }}
       </div>
   </div>
 </template>
@@ -48,6 +48,9 @@ export default {
     }
   },
   computed: {
+    translatedText () {
+      return this.$i18n.i18next.exists(this.text) ? this.$t(this.text) : this.text
+    },
     showIcon () {
       return !_.isEmpty(this.icon)
     },
