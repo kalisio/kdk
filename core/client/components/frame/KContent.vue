106dfab9fdeb2857280ef0d4e1e470270ed399bf
cnouguier <christophe.nouguier@free.fr>
2021-09-02 20:57:20 +0200

    fix: actionRenderer in KContent should take into account 'form-button'

--- core/client/components/frame/KContent.vue
+++ core/client/components/frame/KContent.vue
@@ -41,7 +41,7 @@ export default {
       type: String,
       default: 'button',
       validator: (value) => {
-        return ['button', 'item'].includes(value)
+        return ['button', 'form-button', 'item'].includes(value)
       }
     }
   },
