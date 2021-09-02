ea7e97918a43100b678468adda9e6517abb7d1b3
cnouguier <christophe.nouguier@free.fr>
2021-09-02 18:41:27 +0200

    feat: Provide a role field (close #460)

---
+++ core/client/components/form/KRoleField.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="readOnly" :id="properties.name + '-field'">
+    <q-chip dense  />
+  </div>
+  <q-field v-else
+    :for="properties.name + '-field'"
+    :value="model"
+    :label="label"
+    @input="onChanged"
+    borderless
+    :error-message="errorLabel"
+    :error="hasError"
+    :disabled="disabled"
+    bottom-slots
+  >
+    <!-- Content -->
+    <template v-slot:default>
+      <q-option-group
+        :id="properties.name + '-field'"
+        v-model="model"
+        :options="roles()"
+        inline />
+    </template>
+    <!-- Helper -->
+    <template v-if="helper" v-slot:hint>
+      <span v-html="helper"></span>
+    </template>
+  </q-field>
+</template>
+
+<script>
+import mixins from '../../mixins'
+import { RoleNames } from '../../../common/permissions'
+import { QOptionGroup } from 'quasar'
+
+export default {
+  name: 'k-role-field',
+  components: {
+    QOptionGroup
+  },
+  mixins: [mixins.baseField],
+  methods: {
+    roles () {
+      const roles = [
+        { label: this.$t('MEMBER'), value: 'member' },
+        { label: this.$t('MANAGER'), value: 'manager' },
+        { label: this.$t('OWNER'), value: 'owner' }
+      ]
+      return roles
+    },
+    emptyModel () {
+      return 'member'
+    },
+    isEmpty () {
+      // Can't actually be
+      return false
+    }
+  }
+}
+</script>
