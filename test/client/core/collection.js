c72358d3f9a3585e2c4bc14fef9a911051ef5634
cnouguier <christophe.nouguier@free.fr>
2021-09-17 11:20:13 +0200

    test: added collection function to check whether an item or a card exists

--- test/client/core/collection.js
+++ test/client/core/collection.js
@@ -1,11 +1,24 @@
 import { countElements } from './utils'
 
+
 export async function countItems (page) {
   return countElements(page, `//div[contains(@class, "q-page")]//div[contains(@class, "q-item-type")]`)
 }
 
 export async function countCards (page) {
   return countElements(page, `//div[contains(@class, "q-page")]//div[contains(@class, "q-card")]`)
+}
+
+export async function itemExists (page, name) {
+  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-item-type") and contains(., "${name}")]`
+  const elements = await page.$x(xpath)
+  return elements.length === 1
+}
+
+export async function cardExists (page, name) {
+  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-card") and contains(., "${name}")]`
+  const elements = await page.$x(xpath)
+  return elements.length === 1
 }
 
 export async function clickItemAction (page, name, action, wait = 250) {
