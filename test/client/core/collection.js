dad33b7de01b3d328721be3c8dbb7e13cecc38bb
cnouguier <christophe.nouguier@free.fr>
2021-09-21 16:18:39 +0200

    test: updated collection help functions

--- test/client/core/collection.js
+++ test/client/core/collection.js
@@ -1,27 +1,17 @@
 import { countElements } from './utils'
 
-export async function countItems (page) {
-  return countElements(page, '//div[contains(@class, "q-page")]//div[contains(@class, "q-item-type")]')
+export async function countItems (page, component) {
+  return countElements(page, `//div[contains(@component, "${component}")]`)
 }
 
-export async function countCards (page) {
-  return countElements(page, '//div[contains(@class, "q-page")]//div[contains(@class, "q-card")]')
-}
-
-export async function itemExists (page, name) {
-  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-item-type") and contains(., "${name}")]`
+export async function itemExists (page, component, name) {
+  const xpath = `//div[contains(@component, "${component}") and contains(., "${name}")]`
   const elements = await page.$x(xpath)
   return elements.length === 1
 }
 
-export async function cardExists (page, name) {
-  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-card") and contains(., "${name}")]`
-  const elements = await page.$x(xpath)
-  return elements.length === 1
-}
-
-export async function clickItemAction (page, name, action, wait = 250) {
-  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-item-type") and contains(., "${name}")]//button[@id="${action}"]`
+export async function clickItemAction (page, component, name, action, wait = 250) {
+  const xpath = `//div[contains(@component, "${component}") and contains(., "${name}")]//button[@id="${action}"]`
   const elements = await page.$x(xpath)
   if (elements.length > 0) {
     elements[0].click()
@@ -29,27 +19,22 @@ export async function clickItemAction (page, name, action, wait = 250) {
   }
 }
 
-export async function clickCardAction (page, name, action, wait = 250) {
-  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-card") and contains(., "${name}")]//button[@id="${action}"]`
-  const elements = await page.$x(xpath)
-  if (elements.length > 0) {
-    elements[0].click()
-    await page.waitForTimeout(wait)
-  }
-}
-
-export async function isCardExpanded (page, name) {
-  const xpath = `//div[contains(@class, "q-page")]//div[contains(@class, "q-card") and contains(., "${name}")]//i[contains(@class, "la-angle-up")]`
+export async function isCardExpanded (page, component, name) {
+  const xpath = `//div[contains(@component, "${component}") and contains(., "${name}")]//i[contains(@class, "la-angle-up")]`
   const elements = await page.$x(xpath)
   return elements.length > 0
 }
 
-export async function expandCard (page, name) {
-  const isExpanded = await isCardExpanded(page, name)
-  if (!isExpanded) await clickCardAction(page, name, 'expand-action', 1000)
+export async function expandCard (page, component, name) {
+  const isExpanded = await isCardExpanded(page, component, name)
+  if (!isExpanded) {
+    await clickItemAction(page, component, name, 'expand-action', 1000)
+  }
 }
 
-export async function shrinkCard (page, name) {
-  const isExpanded = await isCardExpanded(page, name)
-  if (isExpanded) await clickCardAction(page, name, 'expand-action', 1000)
+export async function shrinkCard (page, component, name) {
+  const isExpanded = await isCardExpanded(page, component, name)
+  if (isExpanded) {
+    await clickItemAction(page, component, name, 'expand-action', 1000)
+  }
 }
