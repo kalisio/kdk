e5471d90208cbc8556d8970ce03443203820764b
Luc Claustres <luc.claustres@orange.fr>
2021-09-27 18:21:08 +0200

    chore: fixed log in frontend test utilities [skip ci]

--- test/client/core/utils.js
+++ test/client/core/utils.js
@@ -18,12 +18,12 @@ export async function elementExists (page, selector) {
  * see: https://github.com/puppeteer/puppeteer/issues/545
 */
 export async function isElementVisible (page, selector) {
+  debug(`Checking if element ${selector} is visible`)
   return page.evaluate((selector) => {
     const element = document.querySelector(selector)
     if (!element) return false
     const style = window.getComputedStyle(element)
     const visible = (style && style.display !== 'none' && style.visibility !== 'hidden' && style.opacity !== '0')
-    debug(`Element ${selector} ` + (visible ? 'visible' : 'invisible'))
     return visible
   }, selector)
 }
