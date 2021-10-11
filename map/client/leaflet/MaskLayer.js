054f47c549bbb9b43ef66b72868c7df06b517228
cnouguier <christophe.nouguier@free.fr>
2021-10-11 15:31:34 +0200

    wip: Allow to draw a mask using a given GeoJSON polygon #476

--- map/client/leaflet/MaskLayer.js
+++ map/client/leaflet/MaskLayer.js
@@ -5,39 +5,37 @@ import L from 'leaflet'
 const MaskLayer = L.Polygon.extend({
 
   initialize: function (geoJson, options) {
+    // Merge the options
     L.setOptions(this, Object.assign({ 
-      style: {
-        color: "#AAAAAA",
-        weight: 1,
-        'fill-color': "#AAAAAA",
-        'fill-opcatity': 0.8
-      },
+      color: "#AAAAAA",
+      weight: 1,
+      fillColor: "#AAAAAA",
+      fillOpacity: 0.8,
       interactive: false,
       fitBounds: true,
       restrictBounds: true,
       bounds: new L.LatLngBounds([-90, -360], [90, 360])
-      }, options))
-    let outerBoundsLatLngs = [
+      }), options)
+    // Define the outBounds latlngs array of the mask
+    const outerBoundsLatLngs = [
        this.options.bounds.getSouthWest(),
        this.options.bounds.getNorthWest(),
        this.options.bounds.getNorthEast(),
        this.options.bounds.getSouthEast()
     ]
-
+    // Construct the mask
     const type = _.get(geoJson, 'type')
     const geometryType = _.get(geoJson, 'geometry.type')
     const coordinates = _.get(geoJson, 'geometry.coordinates')
-    if ((type !== 'Feature') || (geometryType !== 'Polygon') || !Array.isArray(coordinates)) {
+    let holeCoordinates = []
+    if ((type === 'Feature') && (geometryType === 'Polygon') && Array.isArray(coordinates)) {
+      for (let i=0; i < coordinates[0].length; i++) {
+        holeCoordinates.push(new L.LatLng(coordinates[0][i][1], coordinates[0][i][0]))
+      }
+    } else {
       logger.warn('Invalid/Unsupported GeoJson object for MaskLayer')
-      L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs], options)
     }
-
-    let latlngs = []
-    for (let i=0; i < coordinates[0].length; i++) {
-      latlngs.push(new L.LatLng(coordinates[0][i][1], coordinates[0][i][0]))
-    }
-
-    L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, latlngs], options)
+    L.Polygon.prototype.initialize.call(this, [outerBoundsLatLngs, holeCoordinates], this.options)
   }
 })
 
