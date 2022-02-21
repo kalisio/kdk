f5215b1050607f85b487f581ce7428276167619c
cnouguier <christophe.nouguier@free.fr>
2022-02-21 20:12:56 +0100

    fix: KActionPopup should handle the propagate prop of the KAction (close #558)

--- core/client/components/frame/KPopupAction.vue
+++ core/client/components/frame/KPopupAction.vue
@@ -9,6 +9,7 @@
     :toggle="toggle"
     :toggled="toggled"
     :disabled="disabled"
+    :propagate="propagate"
     :badge="badge"
     :tooltip="tooltip">
     <template v-slot:content>
@@ -90,6 +91,10 @@ export default {
     persistent: {
       type: Boolean,
       default: false
+    },
+    propagate: {
+      type: Boolean,
+      default: true
     },
     content: {
       type: [Object, Array],
