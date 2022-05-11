3855552bf8daf186cd6186b4751e7d1c096caa12
Karl Azemar <karl.azemar@gmail.com>
2022-05-11 15:58:50 +0200

    Moved mapInteraction and zoomToLevel from core/utils.js to map/utils.js

--- test/client/core/utils.js
+++ test/client/core/utils.js
@@ -196,39 +196,9 @@ export async function moveSlider (page, action, direction, times, wait = 250) {
   const selector = `#${action}`;
   const dir = (direction === 'left') ? 'ArrowLeft':'ArrowRight';
   await page.focus(selector)
-  var i = 0;
+  const i = 0;
   for (i = 0; i < times; i++) {
     await page.keyboard.press(dir)
   }
-  await page.waitForTimeout(wait)
-}
-
-/* Moves the map for a specific times: 
-- in a choosen direction (left, right, up, down)
-- zoom in or out
- */
-export async function mapInteraction (page, direction, times, wait = 250) {
-  if (direction === 'up') {var dir = 'ArrowUp'}
-  else if (direction === 'down') {var dir = 'ArrowDown'}
-  else if (direction === 'left') {var dir = 'ArrowLeft'}
-  else if (direction === 'right') {var dir = 'ArrowRight'}
-  else if (direction === 'in') {var dir = '+'}
-  else if (direction === 'out') {var dir = '-'}
-  await page.focus('#map')
-  await page.waitForTimeout(wait)
-  var i = 0;
-  for (i = 0; i < times; i++) {
-    await page.keyboard.press(dir)
-    await page.waitForTimeout(wait)
-  }
-}
-
-/* Zooms the map to a specific level
- */
-export async function zoomToLevel (page, level, wait = 250) {
-  const zoom = await getFromStore(page, 'mapActivity.zoom')
-  const diff = level-zoom;
-  const action = (level > zoom) ? 'in':'out';
-  await mapInteraction(page, action, Math.abs(diff),500)
   await page.waitForTimeout(wait)
 }
---
+++ test/client/map/utils.js
@@ -0,0 +1,32 @@
+import * as core from '../core'
+
+/* Moves the map for a specific times: 
+- in a choosen direction (left, right, up, down)
+- zoom in or out
+ */
+export async function mapInteraction (page, direction, times, wait = 250) {
+  let dir
+  if (direction === 'up') {const dir = 'ArrowUp'}
+  else if (direction === 'down') {const dir = 'ArrowDown'}
+  else if (direction === 'left') {const dir = 'ArrowLeft'}
+  else if (direction === 'right') {const dir = 'ArrowRight'}
+  else if (direction === 'in') {const dir = '+'}
+  else if (direction === 'out') {const dir = '-'}
+  await page.focus('#map')
+  await page.waitForTimeout(wait)
+  var i = 0;
+  for (i = 0; i < times; i++) {
+    await page.keyboard.press(dir)
+    await page.waitForTimeout(wait)
+  }
+}
+
+/* Zooms the map to a specific level
+ */
+export async function zoomToLevel (page, level, wait = 250) {
+  const zoom = await getFromStore(page, 'mapActivity.zoom')
+  const diff = level-zoom;
+  const action = (level > zoom) ? 'in':'out';
+  await mapInteraction(page, action, Math.abs(diff),500)
+  await page.waitForTimeout(wait)
+}
