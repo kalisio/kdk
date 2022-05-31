698ea43ca3b92b50505b190c14aec5d254a74f62
cnouguier <christophe.nouguier@free.fr>
2022-05-31 09:20:01 +0200

    wip: Upgrade to Quasar 2 / Vuejs 3 - i18n refactored (#578)

--- core/client/i18n.js
+++ core/client/i18n.js
@@ -1,16 +1,35 @@
 import { createI18n } from 'vue-i18n'
-import * as utils from './utils.js'
+import { getAppLocale, getAppFallbackLocale } from './utils.js'
+
+// Helper function to load a translation file
+// @i18n alias shoud be added in the quasar.config build section
+async function loadTranslationBundles (bundles, locale, fallbackLocale) {
+  try {
+    const translations = {}
+    translations[locale] = {}
+    translations[fallbackLocale] = {}
+    for (let i = 0; i < bundles.length; i++) {
+      const localeTranslationsModule = await import(`@i18n/${bundles[i]}_${locale}.json`)
+      _.merge(translations[locale], localeTranslationsModule.default)
+      const fallbackTranslationModule = await import(`@i18n/${bundles[i]}_${fallbackLocale}.json`)
+      _.merge(translations[fallbackLocale], fallbackTranslationModule.default)
+    }
+    return translations
+  } catch (error) {
+    logger.error(error)
+  }
+}
 
 export const i18n = {
   async initialize (app, bundles) {
     // Define the locale to be used
-    const fallbackLocale = utils.getAppFallbackLocale()
-    const locale = utils.getAppLocale()
+    const fallbackLocale = getAppFallbackLocale()
+    const locale = getAppLocale()
     // Create i18n instance using the translation bundles
     this.i18n = createI18n({
       locale,
       fallbackLocale,
-      messages: await utils.loadTranslations(bundles, locale, fallbackLocale),
+      messages: await loadTranslationBundles(bundles, locale, fallbackLocale),
       silentFallbackWarn: true
     })
     app.use(this.i18n)
--- core/client/utils.js
+++ core/client/utils.js
@@ -234,25 +234,6 @@ export function getTimezoneLabel (timezone) {
   return `${timezone} (${offset})`
 }
 
-// Helper function to load a translation file
-// @i18n alias shoud be added in the quasar.config build section
-export async function loadTranslations (bundleNames, locale, fallbackLocale) {
-  try {
-    const translations = {}
-    translations[locale] = {}
-    translations[fallbackLocale] = {}
-    for (let i = 0; i < bundleNames.length; i++) {
-      const localeTranslationsModule = await import(`@i18n/${bundleNames[i]}_${locale}.json`)
-      _.merge(translations[locale], localeTranslationsModule.default)
-      const fallbackTranslationModule = await import(`@i18n/${bundleNames[i]}_${fallbackLocale}.json`)
-      _.merge(translations[fallbackLocale], fallbackTranslationModule.default)
-    }
-    return translations
-  } catch (error) {
-    logger.error(error)
-  }
-}
-
 // Helper function to load a schema
 // @schema alias shoud be added in the quasar.config build section
 export async function loadSchema (schemaName) {
