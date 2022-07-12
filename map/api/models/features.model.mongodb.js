fe4715a6b09d7f53046cd4532eceba8ef795dac5
Luc Claustres <luc.claustres@orange.fr>
2022-07-12 20:01:01 +0200

    feat: Improve user-defined feature layers performances by creating appropriate indices (closes #111)

--- map/api/models/features.model.mongodb.js
+++ map/api/models/features.model.mongodb.js
@@ -6,6 +6,11 @@ module.exports = function (app, options) {
   if (options.featureId) {
     options.Model.createIndex({ ['properties.' + options.featureId]: 1 })
   }
+  try {
+    options.Model.createIndex({ 'properties.$**': 1 })
+  } catch (_) {
+    // Fail silently as it might not be supported under v4
+  }
   if (options.expireAfter || options.ttl) {
     options.Model.createIndex({ time: 1 }, { expireAfterSeconds: options.expireAfter || options.ttl })
   }
