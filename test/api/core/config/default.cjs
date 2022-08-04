cc885cfd613577398959011d3f18478405c75220
Luc Claustres <luc.claustres@orange.fr>
2022-08-04 18:47:54 +0200

    chore: fixed typo

--- test/api/core/config/default.cjs
+++ test/api/core/config/default.cjs
@@ -29,7 +29,7 @@ module.exports = {
     },
     jwtOptions: {
       header: {
-        typ: 'access'
+        type: 'access'
       },
       audience: 'https://kalisio.com',
       issuer: 'feathers',
