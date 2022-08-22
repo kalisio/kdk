6e28d414ea67ecfa4dfbe713983d3c3ce3019b42
cnouguier <christophe.nouguier@free.fr>
2022-08-22 09:41:05 +0200

    wip: Upgrade to Quasar 2 / Vuejs 3: lint fixed #578

--- core/client/components/collection/index.js
+++ core/client/components/collection/index.js
@@ -3,7 +3,6 @@ import KCardSection from './KCardSection.vue'
 import KGrid from './KGrid.vue'
 import KItem from './KItem.vue'
 import KList from './KList.vue'
-import KHistoryEntry from './KHistoryEntry.vue'
 import KHistory from './KHistory.vue'
 import KTable from './KTable.vue'
 import KColumn from './KColumn.vue'
@@ -14,7 +13,6 @@ export {
   KGrid,
   KItem,
   KList,
-  KHistoryEntry,
   KHistory,
   KTable,
   KColumn
