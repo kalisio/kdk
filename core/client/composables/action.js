e12d49e992ce5907bcb70f32df9a1ab494a508b0
cnouguier <christophe.nouguier@free.fr>
2022-09-12 17:23:59 +0200

    wip: Allow caching toggle action states #622

--- core/client/composables/action.js
+++ core/client/composables/action.js
@@ -2,31 +2,46 @@ import _ from 'lodash'
 import { ref, readonly, reactive } from 'vue'
 import { Store } from '../store.js'
 
+// states store
 const toggleStates = reactive({})
 
 export function useAction (props) {
   // data
   const isToggled = ref(null)
+
   // functions
   function toggle () {
     isToggled.value = !isToggled.value
-    // cache the value
-    if (props.toggle && props.toggle.store) {
-      Store.patch(props.toggle.store.path, { [props.toggle.store.property]: isToggled.value })
-    } else {
-      toggleStates[props.id] = isToggled.value
-    }
+    // cache the value if required
+    const cache = _.get(props, 'toggle.cache')
+    if (cache) {
+      if (typeof cache === 'string') {
+        // use global store
+        const tokens = cache.split('.')
+        const property = _.last(tokens)
+        const path = _.dropRight(tokens).join('.')
+        Store.patch(path, { [property]: isToggled.value })
+      } else if (typeof cache === 'boolean') {
+        // use local store
+        toggleStates[props.id] = isToggled.value
+      } 
+    } 
   }
+
   // immmediate
-  if (_.has(props, 'toggled')) isToggled.value = props.toggled
-  else {
-    // try to retrieve a cached value
-    if (props.toggle && props.toggle.store) {
-      isToggled.value = Store.get(`${props.toggle.store.path}.${props.toggle.store.property}`, false)
-    } else {
-      isToggled.value = _.get(toggleStates, props.id, false)
-    }
-  } 
+  const cache = _.get(props, 'toggle.cache')
+  if (cache) {
+    if (typeof cache === 'string') {
+      // use global store
+      isToggled.value = Store.get(cache, false)
+    } else if (typeof cache === 'boolean') {
+      // use local store
+      if (cache) isToggled.value = _.get(toggleStates, props.id, false)
+    } 
+  } else {
+    isToggled.value = props.toggled
+  }
+  
   // exposed
   return { 
     isToggled: readonly(isToggled),
