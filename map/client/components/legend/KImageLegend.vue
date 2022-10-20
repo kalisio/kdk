b79570f1aef50a506f54930b7726b499ec101a05
cnouguier <christophe.nouguier@free.fr>
2022-10-20 19:36:35 +0200

    wip: Provide a map legend component #615

--- map/client/components/legend/KImageLegend.vue
+++ map/client/components/legend/KImageLegend.vue
@@ -1,14 +1,35 @@
 <template>
-  <div class="row full-width justify-center">
-    <q-img v-bind="content" />
+  <div class="q-pa-xs row full-width justify-center">
+    <img :src="src" />
   </div>
 </template>
 
 <script setup>
-defineProps({
+import _ from 'lodash'
+import config from 'config'
+import { ref, computed, onMounted } from 'vue'
+import { api } from '../../../../core/client/index.js'
+
+// props
+const props = defineProps({
   content: {
     type: Object,
     required: true
   }
+})
+
+// data
+const jwt = ref(null)
+
+// computed
+const src = computed (() => {
+  const src = props.content.src
+  if (!_.startsWith(src, '/api')) return src 
+  return src + '?jwt=' + jwt.value
+})
+
+// hooks
+onMounted(async () => {
+  jwt.value = await api.get('storage').getItem(config.apiJwt)
 })
 </script>
