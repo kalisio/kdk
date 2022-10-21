2384b3a30598580e3b8f552856039872f26ec13e
cnouguier <christophe.nouguier@free.fr>
2022-10-21 17:19:45 +0200

    wip: Provide a map legend component #615

---
+++ map/client/components/legend/KLegendSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="column full-width q-gutter-xs">
+    <div v-if="label" class="text-weight-light">
+      {{ $tie(label) }}
+    </div> 
+    <slot>
+
+    </slot>
+  </div>
+</template>
+
+<script setup>
+// props
+defineProps({
+  label: {
+    type: String,
+    default: undefined
+  }
+})
+</script>
