d5c4f11ea38cb778c38001f38cc08005e64b94c7
cnouguier <christophe.nouguier@free.fr>
2022-12-08 16:07:33 +0100

    wip; Make various app components more reusable #648

---
+++ core/client/capabilities.js
@@ -0,0 +1,27 @@
+import _ from 'lodash'
+import logger from 'loglevel'
+import config from 'config'
+import { api } from './api.js'
+import { Store } from './store.js'
+
+// Export singleton
+export const Capabilities = {
+  initialize () {
+    window.fetch(api.getBaseUrl() + _.get(config, 'apiPath') + '/capabilities')
+    .then(capabilities => {
+      logger.debug('Fetching api capabilities')
+      capabilities.json()
+      .then(content => {
+        this.content = content
+        // TODO: ensure backward compatibility
+        Store.set('capabilities.api', capabilities)
+        Store.set('capabilities.client', capabilities)
+      })
+    })
+  },
+  get (key) {
+    if (this.content) return _.get(this.content, key)
+    logger.debug('Capabilities not initialized')
+    return
+  }
+}
