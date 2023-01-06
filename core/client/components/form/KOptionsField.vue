62f89ca10051352b93387bdcc3ff553eec14f76c
cnouguier <christophe.nouguier@free.fr>
2023-01-06 17:34:24 +0100

    feat: Provide an options group field (close #668)

---
+++ core/client/components/form/KOptionsField.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="readOnly" :id="properties.name + '-field'">
+    <q-chip dense  />
+  </div>
+  <q-field v-else
+    :for="properties.name + '-field'"
+    :model-value="model"
+    :label="label"
+    borderless
+    :error-message="errorLabel"
+    :error="hasError"
+    :disable="disabled"
+    bottom-slots
+    @update:model-value="onChanged"
+  >
+    <!-- Content -->
+    <template v-slot:control>
+      <q-option-group
+        :id="properties.name + '-field'"
+        v-model="model"
+        :options="options()"
+        inline
+      />
+    </template>
+    <!-- Helper -->
+    <template v-if="helper" v-slot:hint>
+      <span v-html="helper"></span>
+    </template>
+  </q-field>
+</template>
+
+<script>
+import _ from 'lodash'
+import { baseField } from '../../mixins'
+import { QOptionGroup } from 'quasar'
+
+export default {
+  components: {
+    QOptionGroup
+  },
+  mixins: [baseField],
+  methods: {
+    options () {
+      const options = _.get(this.properties, 'field.options', [])
+      return options.map(option => {
+        // Check if we have a translation key or directly the label content
+        const label = _.get(option, 'label', '')
+        return Object.assign({}, option, { label: this.$tie(label) })
+      })
+    }
+  }
+}
+</script>
