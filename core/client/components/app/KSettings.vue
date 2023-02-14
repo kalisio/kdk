6abb92ed47ca1baecc0a072bfa2ca5eb21ed1af1
cnouguier <ch.nouguier@gmail.com>
2023-02-14 11:44:57 +0100

    wip: Make various app components more reusable #648

---
+++ core/client/components/app/KSettings.vue
@@ -0,0 +1,43 @@
+<template>
+  <KForm
+    id="settings"
+    ref="formRef"
+    :values="settings"
+    schema="settings.update"
+    :schema-properties="schemaFilter"
+  />
+</template>
+
+<script setup>
+import _ from 'lodash'
+import { ref, onMounted } from 'vue'
+import { api } from '../../api.js'
+import KForm from '../form/KForm.vue'
+
+// Data
+const serviceSettings = api.getService('settings')
+const formRef = ref(null)
+const settings = ref(null)
+const schemaFilter = ref(null)
+
+// Functions
+async function apply () {
+  const { isValid, values } = formRef.value.validate()
+  if (isValid) {
+    await serviceSettings.patch('settings', values)
+    return true
+  }
+}
+
+// Hooks
+onMounted(async () => {
+  settings.value = await serviceSettings.get('settings')
+  const mapping = serviceSettings.getSettingsMapping()
+  schemaFilter.value = Object.keys(mapping).filter(value => _.get(mapping, value))
+})
+
+// Expose
+defineExpose({
+  apply
+})
+</script>
