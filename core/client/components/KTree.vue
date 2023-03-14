a7f6e6a328d2bccb4ed1286350c2135da678367f
Tristan Greffe <tristan.greffe@hotmail.com>
2023-03-14 13:27:39 +0100

    feat: created a new generic quasar tree component (closes #697) [skip ci]

---
+++ core/client/components/KTree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-tree
+      :nodes="props.nodes"
+      node-key="label"           
+    ></q-tree>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  nodes: {
+    type: Array,
+    default: []
+  },
+})
+
+</script>
