e464e58be8f1e74d1ad00ba6379e38738ff5ce74
cnouguier <ch.nouguier@gmail.com>
2023-03-27 18:07:24 +0200

    wip: Refactor location relative components #676

--- map/client/components/form/KLocationField.vue
+++ map/client/components/form/KLocationField.vue
@@ -1,45 +1,201 @@
 <template>
-  <q-field
+  <div
+    v-if="readOnly"
+    :id="properties.name + '-field'"
+  >
+    {{ model ? model.properties.name : '' }}
+  </div>
+  <q-select
+    v-else
     :for="properties.name + '-field'"
     v-model="model"
     :label="label"
+    clearable
+    use-input
+    hide-dropdown-icon
+    :options="locations"
+    :display-value="model ? model.properties.name : ''"
     :error-message="errorLabel"
     :error="hasError"
-    no-error-icon
+    :disable="disabled"
+    bottom-slots
+    @filter="onSearch"
+    @update:model-value="onChanged"
   >
-    <!-- Content -->
-    <template v-slot:control>
-      <KLocationInput
-        id="location-input"
-        v-model="model"
-        v-bind="properties.field"
-        class="full-width"
-      />
+    <!-- Extra actions -->
+    <template v-slot:append>
+      <div class="q-pl-xs row items-center">
+        <!-- geocoders filtering -->
+        <KAction
+          v-if="allowConfiguration"
+          id="configure"
+          tooltip="KLocationField.FILTER"
+          icon="las la-sliders-h"
+          color="grey-7"
+          size="0.8rem"
+          dense
+        >
+          <q-popup-proxy>
+            <KGeocodersFilter
+              v-model="selectedGeocorders"
+              :geocoders="availableGeocoders"
+            />
+          </q-popup-proxy>
+        </KAction>
+        <!-- geolocation -->
+        <KAction
+          v-if="allowGeolocation"
+          id="geolocate"
+          tooltip="KLocationField.GEOLOCATE"
+          icon="las la-crosshairs"
+          color="grey-7"
+          size="0.8rem"
+          dense
+          :handler="onGeolocated"
+        />
+        <!-- map drawing -->
+        <KAction
+          v-if="allowMap"
+          id="draw"
+          tooltip="KLocationField.DRAW"
+          icon="las la-edit"
+          color="grey-7"
+          size="0.8rem"
+          dense
+          :dialog="{
+            component: 'location/KLocationMap',
+            'v-model': model,
+            draggable: true,
+            header: map,
+            widthPolicy: 'wide',
+            cancelAction: 'CANCEL',
+            okAction: {
+              id: 'ok-button',
+              label: 'APPLY',
+              handler: 'getLocation'
+            },
+            style: 'min-height: 70vh;'
+          }"
+          @dialog-confirmed="onLocationChanged"
+        />
+      </div>
     </template>
-    <!--Helper -->
+    <!-- Selected item -->
+    <template v-slot:selected-item="scope">
+        {{ scope.opt ? scope.opt.properties.name : '' }}
+    </template>
+    <!-- Options -->
+    <template v-slot:option="scope">
+      <q-item v-bind="scope.itemProps" class="option">
+        <q-item-section>
+          <q-item-label>{{ scope.opt.properties.name }}</q-item-label>
+        </q-item-section>
+        <q-tooltip
+          class="q-pa-none"
+          anchor="center end"
+          self="center middle"
+          style="border-radius: 25px;"
+        >
+          <KLocationMap
+            :modelValue="scope.opt"
+            style="min-width: 250px; min-height: 250px; border-radius: 50px;"
+          />
+        </q-tooltip>
+      </q-item>
+    </template>
+    <!-- No options -->
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          {{ $t('KLocationField.NO_RESULTS') }}
+        </q-item-section>
+      </q-item>
+    </template>
+    <!-- Helper -->
     <template v-if="helper" v-slot:hint>
-      <span v-html="helper"></span>
+      <span v-html="helper" />
     </template>
-  </q-field>
+  </q-select>
 </template>
 
 <script>
-import { mixins as kCoreMixins } from '../../../../core/client'
-import KLocationInput from '../KLocationInput.vue'
+import _ from 'lodash'
+import { ref } from 'vue'
+import { mixins as kdkCoreMixins } from '../../../../core/client'
+import { useLocation } from '../../composables'
+import { KAction } from '../../../../core/client/components'
+import KGeocodersFilter from '../location/KGeocodersFilter.vue'
+import KLocationMap from '../location/KLocationMap.vue'
 
 export default {
-  name: 'k-location-field',
   components: {
-    KLocationInput
+    KAction,
+    KGeocodersFilter,
+    KLocationMap
   },
-  mixins: [kCoreMixins.baseField],
+  mixins: [kdkCoreMixins.baseField],
+  computed: {
+    allowConfiguration () {
+      return !_.isEmpty(_.get(this.properties, 'field.geocoders'))
+    },
+    allowGeolocation () {
+      return _.get(this.properties, 'field.geolocate', true)
+    },
+    allowMap () {
+      return this.map !== null
+    },
+    map () {
+      return _.get(this.properties, 'field.map')
+    }
+  },
   methods: {
     emptyModel () {
       return null
     },
-    fill (value) {
-      this.model = value
+    async onSearch (pattern, update, abort) {
+      if (pattern.length < 4) {
+        abort()
+        return
+      }
+      const locations = await this.search(pattern)
+      update(() => { 
+        this.locations = locations
+      })
+    },
+    async onGeolocated () {
+      const location = await this.geolocate()
+      if (location) {
+        this.fill(location)
+        this.onChanged()
+      }
+    },
+    onLocationChanged (context, location) {
+      this.model = location
+      return true
     }
+  },
+  setup () {
+    // Data
+    const { search, geolocate } = useLocation()
+    const locations = ref([])
+    const availableGeocoders = ref([])
+    const selectedGeocorders = ref([])
+    // Expose
+    return {
+      locations,
+      availableGeocoders,
+      selectedGeocorders,
+      geolocate,
+      search
+    }
+  },
+  mounted () {
+    this.availableGeocoders = _.filter(_.get(this.properties, 'field.geocoders'), geocoder => {
+      return _.get(geocoder, 'selectable', true)
+    })
+    this.selectedGeocorders = _.map(this.availableGeocoders, geocoder => {
+      return geocoder.value
+    })
   }
 }
 </script>
