f13ce30d5fdb0f5ceea2a80d8de92b3cbb039077
cnouguier <ch.nouguier@gmail.com>
2023-03-28 11:37:29 +0200

    feat: Provide a input component of type of CRON (close #703)

---
+++ core/client/components/form/KCronField.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="readOnly" :id="properties.name + '-field'">
+    {{ model }}
+  </div>
+  <q-input v-else
+    :for="properties.name + '-field'"
+    type="text"
+    v-model="model"
+    :label="label"
+    clearable
+    :disable="disabled"
+    :error="hasError"
+    :error-message="errorLabel"
+    :rules="validationRules"
+    bottom-slots
+    @blur="onChanged"
+    @update:model-value='onChanged'
+  >
+    <!-- Helper -->
+    <template v-if="helper" v-slot:hint>
+      <span v-html="helper"></span>
+    </template>
+  </q-input>
+</template>
+
+<script>
+import _ from 'lodash'
+import { baseField } from '../../mixins'
+
+export default {
+  mixins: [baseField],
+  computed: {
+    validationRules () {
+      const cronRegExp = /(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/g
+      return [
+        (value) => cronRegExp.test(value) || 'Invalid CRON expression' 
+      ]
+    }
+  }
+}
+</script>
