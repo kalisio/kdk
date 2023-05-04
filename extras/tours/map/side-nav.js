1e6bfea08588b32f39310ab5250e54b36bdfc61e
Tristan Greffe <tristan.greffe@hotmail.com>
2023-05-04 11:53:29 +0200

    chore: create generic tour side-nav

---
+++ extras/tours/map/side-nav.js
@@ -0,0 +1,35 @@
+module.exports = [{
+  target: '#logout',
+  title: 'tours.side-nav.LOGOUT_LABEL',
+  content: 'tours.side-nav.SESSION_LABEL',
+  params: {
+    placement: 'top',
+    clickOnNext: '#left-opener'
+  }
+}, {
+  target: '#contextual-help',
+  content: 'tours.side-nav.CONTEXT_HELP_LABEL',
+  params: {
+    placement: 'top',
+    clickOnPrevious: '#left-opener',
+    clickOnNext: '#left-opener'
+  }
+}, {
+  target: '#about',
+  content: 'tours.side-nav.ABOUT_LABEL',
+  params: {
+    placement: 'top',
+    clickOnPrevious: '#left-opener',
+    clickOnNext: ['#left-opener', '#about'],
+    nextDelay: 500
+  }
+}, {
+  target: '#report-bug',
+  content: 'tours.side-nav.BUG_LABEL',
+  params: {
+    placement: 'top',
+    clickOnPrevious: ['#ok-action', '#left-opener'],
+    clickOnNext: '#ok-action',
+    previousDelay: 500
+  }
+}]
