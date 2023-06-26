7ec704e4ef9eee9650c10c81f98425793f4055ba
Tristan Greffe <tristan.greffe@hotmail.com>
2023-06-26 15:30:47 +0200

    chore: replace button with dialog for installation request

--- core/client/components/KInstallApp.vue
+++ core/client/components/KInstallApp.vue
@@ -1,14 +1,16 @@
 <template>
-  <div v-if="showInstallApp" class="row justify-center">
-    <KAction
-      id="install-app"
-      renderer="form-button"
-      color="primary"
-      size="sm"
-      label="pwa.BUTTON_INSTALL"
-      :handler="installApp"
-    />
-  </div>
+  <q-dialog id="install-app" v-model="showInstallApp" persistent position="bottom">
+    <q-card class="bg-info q-pa-sm" style="width: 350px; color: white">
+      <q-card-section class="row justify-between">
+        <q-icon name="info" size="sm"/>
+        <span class="text-body1" text-color="white">{{$t('pwa.MESSAGE_INSTALL')}}</span>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat color="white" :label="$t('pwa.BUTTON_INSTALL')" @click="installApp" />
+        <q-btn flat color="white" :label="$t('pwa.BUTTON_DISMISS')" v-close-popup  />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup>
@@ -27,6 +29,8 @@ async function installApp () {
   // Wait for the user to respond to the prompt
   const { outcome } = await deferredPrompt.userChoice
   logger.debug(`User response to the install prompt: ${outcome}`)
+  // Refresh page
+  if (outcome === 'accepted') location.reload()
 }
 function installAppAvailable () {
   window.addEventListener('beforeinstallprompt', (e) => {
