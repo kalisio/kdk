79507a29007b070d1cb9be1a6ea2d4807abb1549
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-11 11:25:43 +0200

    chore: added dialog to install application on ios #263

--- core/client/components/KInstallApp.vue
+++ core/client/components/KInstallApp.vue
@@ -1,12 +1,19 @@
 <template>
-  <q-dialog id="install-app" v-model="showInstallApp" persistent position="bottom">
+  <q-dialog id="install-app" v-model="showInstallApp.show" persistent position="bottom">
     <q-card class="bg-info q-pa-sm" style="width: 350px; color: white">
       <q-card-section class="row justify-between">
         <q-icon name="info" size="sm"/>
-        <span class="text-body1" text-color="white">{{$t('pwa.MESSAGE_INSTALL')}}</span>
+        <span class="text-body1" text-color="white">
+          {{ $t('pwa.MESSAGE_INSTALL') }}
+          <div v-if="showInstallApp.isIos">
+            {{ $t('pwa.MESSAGE_INSTALL_IOS_SHARE_ICON') }}
+            <q-icon name="ios_share" size="18px"/>
+            {{ $t('pwa.MESSAGE_INSTALL_IOS_HOME_SCREEN') }}
+          </div>
+        </span>
       </q-card-section>
       <q-card-actions align="right">
-        <q-btn flat color="white" :label="$t('pwa.BUTTON_INSTALL')" @click="installApp" />
+        <q-btn v-if="!showInstallApp.isIos" flat color="white" :label="$t('pwa.BUTTON_INSTALL')" @click="installApp" />
         <q-btn flat color="white" :label="$t('CLOSE')" v-close-popup  />
       </q-card-actions>
     </q-card>
@@ -16,12 +23,16 @@
 <script setup>
 import { ref } from 'vue'
 import logger from 'loglevel'
+import { Platform } from 'quasar'
 
 // Data
-const showInstallApp = ref(false)
+const showInstallApp = ref({
+  isIos: false,
+  show: false
+})
 let deferredPrompt = null
 
-// functions
+// Functions
 async function installApp () {
   showInstallApp.value = false
   // Show the install prompt
@@ -33,15 +44,22 @@ async function installApp () {
   if (outcome === 'accepted') location.reload()
 }
 function installAppAvailable () {
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault()
-    // Stash the event so it can be triggered later
-    deferredPrompt = e
-    // Show the button install app
-    showInstallApp.value = true
-  })
+  if (Platform.is.ios) {
+    showInstallApp.value = {
+      isIos: true,
+      show: true
+    }
+  } else {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault()
+      // Stash the event so it can be triggered later
+      deferredPrompt = e
+      // Show the button install app
+      showInstallApp.value.show = true
+    })
+  }
 }
 
-// immediate
+// Immediate
 installAppAvailable()
 </script>
