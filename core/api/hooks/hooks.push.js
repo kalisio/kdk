e12e6505b68406d94cb1fce14b8359e48f3ec2f4
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-18 22:32:46 +0200

    wip: send an email when subscribing to webpush #263

---
+++ core/api/hooks/hooks.push.js
@@ -0,0 +1,49 @@
+import emails from 'email-templates'
+import path from 'path'
+import makeDebug from 'debug'
+import _ from 'lodash'
+
+const debug = makeDebug('kdk:core:push:hooks')
+
+export async function sendNewSubscriptionEmail (hook) {
+  if (hook.type !== 'after') {
+    throw new Error('The \'sendNewSubscriptionEmail\' hook should only be used as a \'after\' hook.')
+  }
+
+  // Check for a new subscription
+  const updatedUser = hook.result
+  const previousUser = hook.params.user
+  const newSubscription = _.differenceBy(_.get(updatedUser, 'subscriptions', []), _.get(previousUser, 'subscriptions', []), 'endpoint')
+  if (_.size(newSubscription) !== 1) return
+
+  // Data
+  const app = hook.app
+  const mailerService = app.getService('mailer')
+  const domainPath = app.get('domain') + '/#/'
+  const email = {
+    subject: 'Security alert - new browser detected',
+    from: mailerService.options.auth.user,
+    // When changing email send to the new one so that it can be verified
+    to: updatedUser.email,
+    link: domainPath,
+    domainPath
+  }
+
+  // Build the subject & link to the app to perform the different actions
+  const templateDir = path.join(mailerService.options.templateDir, 'newSubscription')
+  const template = new emails.EmailTemplate(templateDir)
+  // Errors does not seem to be correctly catched by the caller
+  // so we catch them here to avoid any problem
+  try {
+    const emailContent = await template.render({ email, user: updatedUser, subscription: _.first(newSubscription) }, updatedUser.locale || 'en-us')
+    // Update compiled content
+    email.html = emailContent.html
+    debug('Sending email ', email)
+    await mailerService.create(email)
+  } catch (error) {
+    debug('Sending email failed', error)
+    app.logger.error(error)
+  }
+
+  return hook
+}
