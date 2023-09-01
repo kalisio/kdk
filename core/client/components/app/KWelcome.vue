c1874543f7b07e7d6485ff448c38a80c6cee4713
cnouguier <ch.nouguier@gmail.com>
2023-09-01 15:34:39 +0200

    wip: KWelcome is not well rendered on mobile devices #752

--- core/client/components/app/KWelcome.vue
+++ core/client/components/app/KWelcome.vue
@@ -1,57 +1,71 @@
 <template>
   <q-dialog v-model="showWelcome" persistent>
-    <q-card class="q-pa-sm column items-center q-gutter-y-sm">
+    <q-card class="q-pa-xs column items-center q-gutter-y-xs no-wrap">
       <!-- Logo -->
-      <component :is="logoComponent" />
+      <component 
+        :is="logoComponent" 
+        :height="dense ? '70px' : '100px'"
+      />
       <!-- Carousel -->
       <q-carousel
-        class="q-pa-none"
         v-model="currentSlide"
         swipeable
         animated
-        padding
         navigation
         transition-prev="scale"
         transition-next="scale"
         control-type="flat"
         control-color="primary"
-        height="300px"
+        height="320px"
       >
-        <q-carousel-slide name="welcome" class="column no-wrap justify-center text-center q-gutter-md">
-          <div class="text-h5">{{ $t('KWelcome.WELCOME_TITLE') }}</div>
-          <div class="text-subtitle1">{{ $t('KWelcome.WELCOME_MESSAGE') }}</div>
-          <div class="text-subtitle1">{{ $t('KWelcome.ONLINE_HELP') }}
+        <q-carousel-slide 
+          name="welcome" 
+          class="column no-wrap justify-center text-center q-gutter-sm"
+        >
+          <div :class="dense ? 'text-weight-medium' : 'text-h6'">{{ $t('KWelcome.WELCOME_TITLE') }}</div>
+          <div>{{ $t('KWelcome.WELCOME_MESSAGE') }}</div>
+          <div>{{ $t('KWelcome.ONLINE_HELP') }}
             <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onOnlineHelp()"/>
           </div>
         </q-carousel-slide>
-        <q-carousel-slide name="tour" class="column no-wrap justify-center text-center q-gutter-md">
-          <div class="text-subtitle1">{{ $t('KWelcome.TOUR_MESSAGE') }}
+        <q-carousel-slide 
+          name="tour" 
+          class="column no-wrap justify-center text-center q-gutter-sm"
+        >
+          <div>{{ $t('KWelcome.TOUR_MESSAGE') }}
             <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onDefaultTour()"/>
           </div>
-          <div class="text-subtitle1">{{ $t('KWelcome.TOUR_LINK_MESSAGE') }}</div>
+          <div>{{ $t('KWelcome.TOUR_LINK_MESSAGE') }}</div>
         </q-carousel-slide>
-        <q-carousel-slide name="goodbye" class="column no-wrap justify-center text-center q-gutter-md">
-          <div class="text-subtitle1">
+        <q-carousel-slide 
+          name="goodbye" 
+          class="column no-wrap justify-center text-center q-gutter-sm"
+        >
+          <div>
             <span>
               {{ $t('KWelcome.CONTEXTUAL_HELP') }}
               <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-question-circle"/>
             </span>
           </div>
-          <div class="text-subtitle1">{{ $t('KWelcome.GOODBYE_MESSAGE') }}</div>
+          <div>{{ $t('KWelcome.GOODBYE_MESSAGE') }}</div>
         </q-carousel-slide>
       </q-carousel>
-      <KAction  
-        id="close-button" 
-        label="CLOSE" 
-        renderer="form-button" 
-        :handler="() => hide()" 
-      />
+      <div :class="dense ? 'q-py-xs' : 'q-py-md'">
+        <KAction  
+          id="close-button" 
+          label="CLOSE" 
+          renderer="form-button" 
+          :handler="() => hide()" 
+        />
+      </div>
       <q-checkbox 
         v-model="toggle" 
         :label="$t('KWelcome.HIDE_WELCOME')" 
         color="primary"
         size="xs"
-        @update:modelValue="onToggleIntroduction" 
+        :dense="dense"
+        @update:modelValue="onToggleIntroduction"
+        :class="dense ? 'q-pt-xs text-caption' : 'q-pt-md'"
       />
     </q-card>
   </q-dialog>
@@ -60,18 +74,24 @@
 <script setup>
 import _ from 'lodash'
 import config from 'config'
-import { ref, onBeforeUnmount } from 'vue'
-import { openURL } from 'quasar'
+import { ref, computed, onBeforeUnmount } from 'vue'
+import { openURL, useQuasar } from 'quasar'
 import { Store, api } from '../..'
 import { loadComponent } from '../../utils'
 import KAction from '../KAction.vue'
 
 // Data
+const $q = useQuasar()
 const logoComponent = ref(loadComponent(_.get(config, 'logoComponent', 'KLogo')))
 const defaultTour = _.get(config, 'welcome.tour', 'home')
 const showWelcome = ref(false)
 const currentSlide = ref('welcome')
 const toggle = ref(false)
+
+// Computed
+const dense = computed(() => {
+  return $q.screen.lt.sm
+})
 
 // functions
 function getWelcomeKey () {
