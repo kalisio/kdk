3eb3149ba4dc4687c8e455363e1031812e64a7f0
cnouguier <ch.nouguier@gmail.com>
2023-09-01 09:40:37 +0200

    fix: KWelcome is not well rendered on mobile devices 'close #752)

--- core/client/components/app/KWelcome.vue
+++ core/client/components/app/KWelcome.vue
@@ -1,52 +1,58 @@
 <template>
   <q-dialog v-model="showWelcome" persistent>
-    <q-card class="q-pa-sm column q-gutter-y-md" style="min-width: 50vw">
+    <q-card class="q-pa-sm column items-center q-gutter-y-sm">
       <!-- Logo -->
       <component :is="logoComponent" />
       <!-- Carousel -->
-      <div class="row justify-center">
-        <q-carousel
-          class="q-pa-none"
-          v-model="currentSlide"
-          swipeable
-          animated
-          padding
-          navigation
-          transition-prev="scale"
-          transition-next="scale"
-          control-type="flat"
-          control-color="primary"
-        >
-          <q-carousel-slide name="welcome" class="column no-wrap justify-center text-center q-gutter-md">
-            <div class="text-h5">{{ $t('KWelcome.WELCOME_TITLE') }}</div>
-            <div class="text-subtitle1">{{ $t('KWelcome.WELCOME_MESSAGE') }}</div>
-            <div class="text-subtitle1">{{ $t('KWelcome.ONLINE_HELP') }}
-              <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onOnlineHelp()"/>
-            </div>
-          </q-carousel-slide>
-          <q-carousel-slide name="tour" class="column no-wrap justify-center text-center q-gutter-md">
-            <div class="text-subtitle1">{{ $t('KWelcome.TOUR_MESSAGE') }}
-              <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onDefaultTour()"/>
-            </div>
-            <div class="text-subtitle1">{{ $t('KWelcome.TOUR_LINK_MESSAGE') }}</div>
-          </q-carousel-slide>
-          <q-carousel-slide name="goodbye" class="column no-wrap justify-center text-center q-gutter-md">
-            <div class="text-subtitle1">
-              <span>
-                {{ $t('KWelcome.CONTEXTUAL_HELP') }}
-                <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-question-circle"/>
-              </span>
-            </div>
-            <div class="text-subtitle1">{{ $t('KWelcome.GOODBYE_MESSAGE') }}</div>
-          </q-carousel-slide>
-        </q-carousel>
-      </div>
-      <div class="row justify-center">
-        <k-action id="close-button" label="CLOSE" renderer="form-button" :handler="() => hide()" />
-      </div>
-      <div class="row justify-center">
-        <q-checkbox v-model="toggle" @update:modelValue="onToggleIntroduction" :label="$t('KWelcome.HIDE_WELCOME')" color="primary" />
-      </div>
+      <q-carousel
+        class="q-pa-none"
+        v-model="currentSlide"
+        swipeable
+        animated
+        padding
+        navigation
+        transition-prev="scale"
+        transition-next="scale"
+        control-type="flat"
+        control-color="primary"
+        height="300px"
+      >
+        <q-carousel-slide name="welcome" class="column no-wrap justify-center text-center q-gutter-md">
+          <div class="text-h5">{{ $t('KWelcome.WELCOME_TITLE') }}</div>
+          <div class="text-subtitle1">{{ $t('KWelcome.WELCOME_MESSAGE') }}</div>
+          <div class="text-subtitle1">{{ $t('KWelcome.ONLINE_HELP') }}
+            <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onOnlineHelp()"/>
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tour" class="column no-wrap justify-center text-center q-gutter-md">
+          <div class="text-subtitle1">{{ $t('KWelcome.TOUR_MESSAGE') }}
+            <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-external-link-square-alt" @click="onDefaultTour()"/>
+          </div>
+          <div class="text-subtitle1">{{ $t('KWelcome.TOUR_LINK_MESSAGE') }}</div>
+        </q-carousel-slide>
+        <q-carousel-slide name="goodbye" class="column no-wrap justify-center text-center q-gutter-md">
+          <div class="text-subtitle1">
+            <span>
+              {{ $t('KWelcome.CONTEXTUAL_HELP') }}
+              <q-icon size="1.5em" class="text-primary cursor-pointer" name="las la-question-circle"/>
+            </span>
+          </div>
+          <div class="text-subtitle1">{{ $t('KWelcome.GOODBYE_MESSAGE') }}</div>
+        </q-carousel-slide>
+      </q-carousel>
+      <KAction  
+        id="close-button" 
+        label="CLOSE" 
+        renderer="form-button" 
+        :handler="() => hide()" 
+      />
+      <q-checkbox 
+        v-model="toggle" 
+        :label="$t('KWelcome.HIDE_WELCOME')" 
+        color="primary"
+        size="xs"
+        @update:modelValue="onToggleIntroduction" 
+      />
     </q-card>
   </q-dialog>
 </template>
