1e3e1d96fcb4a165c61941a4f916b6e3ebeaa0c2
Luc Claustres <luc.claustres@orange.fr>
2023-09-07 14:47:09 +0200

    chore: improved test runner to manage PWA notifications and console messages

--- test/client/core/layout.js
+++ test/client/core/layout.js
@@ -103,6 +103,10 @@ export async function closeWelcomeDialog (page) {
   await click(page, '.q-dialog #close-button')
 }
 
+export async function closeInstallDialog (page) {
+  await click(page, '.q-dialog #ignore-button')
+}
+
 export async function isToastVisible (page) {
   return isElementVisible(page, '[role="alert"]')
 }
