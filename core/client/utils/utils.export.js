aa02cfc815c504c1a0fad3f6111fa18c5e1278da
cnouguier <ch.nouguier@gmail.com>
2023-10-25 23:04:16 +0200

    feat: Provide an export tool (close #787)

--- core/client/utils/utils.export.js
+++ core/client/utils/utils.export.js
@@ -1,21 +1,47 @@
 import _ from 'lodash'
-import { Dialog } from 'quasar'
+import logger from 'loglevel'
+import { Dialog, Notify } from 'quasar'
 import { api } from '../api.js'
 import { i18n } from '../i18n.js'
 
 async function callService (options) {
-  let response = await api.getService('import-export').create({ 
-    method: 'export',
-    servicePath: api.getServicePath(options.service, options.context).substr(1),
-    query: options.query,
-    format: options.format,
-    gzip: options.gzip
-  })
-  // open the returned signed url
-  window.open(response.SignedUrl, options.format === 'json' ? '_blank' : '_self')
+  let dismiss 
+  try {
+    dismiss = Notify.create({
+      group: 'export',
+      icon: 'las la-hourglass-half',
+      message: i18n.t('utils.export.EXPORTING'),
+      color: 'primary',
+      timeout: 0,
+      spinner: true
+    })
+    console.log(dismiss)
+    const response = await api.getService('import-export').create({ 
+      method: 'export',
+      servicePath: api.getServicePath(options.service, options.context).substr(1),
+      query: options.query,
+      format: options.format,
+      gzip: options.gzip
+    })
+    dismiss()
+    if (response.SignedUrl) {
+      // open the returned signed url
+      window.open(response.SignedUrl, options.format === 'json' ? '_blank' : '_self')
+    } else {
+      Events.emit('error', { message: this.$t('errors.' + response.status) })
+    }
+  } catch (error) {
+    dismiss()
+    Events.emit('error', { message: this.$t('errors.NETWORK_ERROR') })
+  }
 }
   
-export async function create (options) {
+export async function createExport (options) {
+  if (!options.service) {
+    logger.error(`[KDK] invalid options: missing 'service' property`)
+    return
+  }
+  logger.debug(`[KDK] creating export with options ${JSON.stringify(options, null, 2)}`)
   if (options.formats.length > 1) {
     const dialog = {
       title: i18n.t(options.title ? options.title : 'utils.export.TITLE'),
