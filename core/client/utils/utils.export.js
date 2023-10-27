a9b62af59d7e12c64c1464a04df9d2240e270fcc
cnouguier <ch.nouguier@gmail.com>
2023-10-27 18:38:10 +0200

    wip: Provide an export tool #787

--- core/client/utils/utils.export.js
+++ core/client/utils/utils.export.js
@@ -1,35 +1,31 @@
 import _ from 'lodash'
 import logger from 'loglevel'
-import { Dialog, Notify } from 'quasar'
+import { Dialog, Notify, openURL } from 'quasar'
 import { Events } from '../events.js'
 import { api } from '../api.js'
 import { i18n } from '../i18n.js'
 
 async function callService (options) {
-  let dismiss 
+  logger.debug(`[KDK] creating export with options ${JSON.stringify(options, null, 2)}`)
+  const dismiss = Notify.create({
+    group: 'export',
+    icon: 'las la-hourglass-half',
+    message: i18n.t('utils.export.EXPORTING'),
+    color: 'primary',
+    timeout: 0,
+    spinner: true
+  })
   try {
-    dismiss = Notify.create({
-      group: 'export',
-      icon: 'las la-hourglass-half',
-      message: i18n.t('utils.export.EXPORTING'),
-      color: 'primary',
-      timeout: 0,
-      spinner: true
-    })
     const response = await api.getService('import-export').create({ 
       method: 'export',
-      servicePath: api.getServicePath(options.service, options.context),
+      servicePath: api.getServicePath(options.service, options.context).substring(1),
       query: options.query,
       format: options.format,
       gzip: options.gzip
     })
     dismiss()
-    if (response.SignedUrl) {
-      // open the returned signed url
-      window.open(response.SignedUrl, options.format === 'json' ? '_blank' : '_self')
-    } else {
-      Events.emit('error', { message: i18n.t('errors.' + response.status) })
-    }
+    if (response.SignedUrl) openURL(response.SignedUrl)
+    else Events.emit('error', { message: i18n.t('errors.' + response.status) })
   } catch (error) {
     dismiss()
     Events.emit('error', { message: i18n.t('errors.NETWORK_ERROR') })
@@ -41,7 +37,6 @@ export async function createExport (options) {
     logger.error(`[KDK] invalid options: missing 'service' property`)
     return
   }
-  logger.debug(`[KDK] creating export with options ${JSON.stringify(options, null, 2)}`)
   if (options.formats.length > 1) {
     const dialog = {
       title: i18n.t(options.title ? options.title : 'utils.export.TITLE'),
@@ -66,9 +61,9 @@ export async function createExport (options) {
     }
     Dialog.create(dialog)
     .onOk(async (format) => {
-      await callService(Object.assign(options, { format }))
+      await callService(Object.assign({ format }, options))
     })
   } else {
-    await callService(Object.assign(options, { format: options.formats[0] }))
+    await callService(Object.assign({ format: options.formats[0] }, options))
   }
 }
