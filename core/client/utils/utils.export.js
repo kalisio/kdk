7fede531c5b05b308ef6920193966ca1ba8c2633
cnouguier <ch.nouguier@gmail.com>
2023-10-30 08:59:58 +0100

    wip: Provide an export tool #787

--- core/client/utils/utils.export.js
+++ core/client/utils/utils.export.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import logger from 'loglevel'
-import { Dialog, Notify, openURL } from 'quasar'
+import { Dialog, Notify } from 'quasar'
 import { Events } from '../events.js'
 import { api } from '../api.js'
 import { i18n } from '../i18n.js'
@@ -17,9 +17,13 @@ async function callService (options) {
   })
   try {
     const servicePath = api.getServicePath(options.service, options.context).substring(1)
-    const response = await api.getService('import-export').create( Object.assign(options, { method: 'export', servicePath }))
+    const transform = _.get(options, 'transform.' + options.format)
+    const response = await api.getService('import-export').create( Object.assign(options, { method: 'export', servicePath, transform }))
     dismiss()
-    if (response.SignedUrl) openURL(response.SignedUrl)
+    // Because of the Content-Disposition, it force the browser to download the file
+    // So just open the link with the _selft target. 
+    // Do not use Quasar openURL as it opens a new window (target is _blank)
+    if (response.SignedUrl) window.open(response.SignedUrl, '_self') 
     else Events.emit('error', { message: i18n.t('errors.' + response.status) })
   } catch (error) {
     dismiss()
