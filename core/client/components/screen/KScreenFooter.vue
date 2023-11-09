32d77bc409ea40797069608461a7f9ba8c26db9a
cnouguier <ch.nouguier@gmail.com>
2023-11-09 20:09:49 +0100

    chore: removed changed endpoint functionnalities

--- core/client/components/screen/KScreenFooter.vue
+++ core/client/components/screen/KScreenFooter.vue
@@ -2,15 +2,6 @@
   <div class="q-pa-sm column justify-center q-gutter-sm">
     <!-- Version -->
     <KVersion />
-    <!-- Endpoint -->
-    <div v-if="canChangeEndpoint" class="row justify-center">
-      <KAction
-        class="text-caption"
-        id="change-endpoint"
-        label="KScreen.CHANGE_ENDPOINT_LINK"
-        :route="{name: 'change-endpoint'}"
-      />
-    </div>
     <!-- Sponsor -->
     <KSponsor />
   </div>
@@ -25,6 +16,4 @@ import KSponsor from '../KSponsor.vue'
 
 // Data
 const { Version } = useVersion()
-const canChangeEndpoint = ref(false)
-if (Version.value.flavor === 'dev') canChangeEndpoint.value = true
 </script>
