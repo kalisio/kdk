279cc84f742a163bd273fecdfda55098220f9df7
tristan-greffe <tristan.greffe@hotmail.com>
2023-11-16 02:14:42 +0100

    wip: improve KCaptureToolbar component #792

--- map/client/components/KCaptureToolbar.vue
+++ map/client/components/KCaptureToolbar.vue
@@ -41,115 +41,14 @@
       id="capture-button"
       icon="las la-camera"
       tooltip="KCaptureToolbar.CAPTURE_ACTION"
-      :handler="() => this.capture()"
+      :handler="() => capture()"
     />
   </div>
 </template>
 
-<script>
-import _ from 'lodash'
+<script setup>
 import KAction from '../../../core/client/components/KAction.vue'
-import { Events, Time } from '../../../core/client'
-import { exportFile } from 'quasar'
+import { useCapture } from '../composables/capture.js'
 
-export default {
-  name: 'k-capture-toolbar',
-  components: {
-    KAction
-  },
-  inject: ['kActivity'],
-  data () {
-    return {
-      width: 1280,
-      height: 720,
-      resolution: undefined
-    }
-  },
-  watch: {
-    resolution: {
-      immediate: true,
-      handler (value) {
-        if (!value) this.resolution = this.getResolutions()[1]
-        else {
-          const size = _.split(this.resolution.value, 'x')
-          this.width = size[0]
-          this.height = size[1]
-        }
-      }
-    },
-    width: {
-      handler (value) {
-        if (value < 256) this.width = 256
-        if (value > 4000) this.width = 4000
-      }
-    },
-    height: {
-      handler (value) {
-        if (value < 256) this.height = 256
-        if (value > 4000) this.height = 4000
-      }
-    }
-  },
-  methods: {
-    getResolutions () {
-      return [
-        { label: this.$t('KCaptureToolbar.SD_LABEL'), description: this.$t('KCaptureToolbar.SD_DESCRIPTION'), value: '640x480' },
-        { label: this.$t('KCaptureToolbar.HD_LABEL'), description: this.$t('KCaptureToolbar.HD_DESCRIPTION'), value: '1280x720' },
-        { label: this.$t('KCaptureToolbar.FHD_LABEL'), description: this.$t('KCaptureToolbar.FHD_DESCRIPTION'), value: '1920x1080' },
-        { label: this.$t('KCaptureToolbar.QHD_LABEL'), description: this.$t('KCaptureToolbar.QHD_DESCRIPTION'), value: '2560x1440' },
-        { label: this.$t('KCaptureToolbar.2K_LABEL'), description: this.$t('KCaptureToolbar.2K_DESCRIPTION'), value: '2048x1080' },
-        { label: this.$t('KCaptureToolbar.4K_LABEL'), description: this.$t('KCaptureToolbar.4K_DESCRIPTION'), value: '3840x2160' }
-      ]
-    },
-    async capture () {
-      // Retrieve the layers
-      const layers = this.kActivity.getContextParameters('layers').layers
-      // Retrieve the extension
-      const bbox = this.kActivity.getContextParameters('view')
-      // Setup the request url options
-      const endpoint = this.$store.get('capabilities.api.gateway') + '/capture'
-      const options = {
-        method: 'POST',
-        mode: 'cors',
-        body: JSON.stringify({
-          activity: this.kActivity.is3D() ? 'globe' : 'map',
-          layers,
-          bbox: [bbox.west, bbox.south, bbox.east, bbox.north],
-          size: { width: +this.width, height: +this.height },
-          time: Time.getCurrentTime().toISOString()
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-      // Add the Authorization header if jwt is defined
-      const jwt = await this.$api.get('storage').getItem(this.$config('gatewayJwt'))
-      if (jwt) options.headers.Authorization = 'Bearer ' + jwt
-      // Perform the request
-      let dismiss = null
-      try {
-        dismiss = this.$q.notify({
-          group: 'capture',
-          icon: 'las la-hourglass-half',
-          message: this.$t('KCaptureToolbar.CAPTURING_VIEW'),
-          color: 'primary',
-          timeout: 0,
-          spinner: true
-        })
-        const response = await fetch(endpoint, options)
-        dismiss()
-        if (response.ok) {
-          const arrayBuffer = await response.arrayBuffer()
-          exportFile('capture.png', new Uint8Array(arrayBuffer))
-        } else {
-          Events.emit('error', { message: this.$t('errors.' + response.status) })
-        }
-      } catch (error) {
-        // Network error
-        dismiss()
-        Events.emit('error', { message: this.$t('errors.NETWORK_ERROR') })
-      }
-    }
-  }
-}
+const { width, height, resolution, getResolutions, capture } = useCapture()
 </script>
