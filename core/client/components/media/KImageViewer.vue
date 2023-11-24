f64e103ca209de79c2d87db6b9cfa8a9b6e28647
cnouguier <ch.nouguier@gmail.com>
2023-11-24 18:47:52 +0100

    chore: refactor KImageView to composition API

--- core/client/components/media/KImageViewer.vue
+++ core/client/components/media/KImageViewer.vue
@@ -1,44 +1,63 @@
 <template>
-  <div v-if="interactive === true" class="fit" >
-    <pinch-zoom class="pinch-zoom-controller fit row justify-center items-center" @change="$emit('image-transformed')">
+  <div>
+    <div v-if="interactive === true" class="fit" >
+      <pinch-zoom class="pinch-zoom-controller fit row justify-center items-center" @change="$emit('image-transformed')">
+        <img :src="source" style="max-width:100%; max-height:100%" :onload="onLoaded" />
+      </pinch-zoom>
+      
+    </div>
+    <div v-else class="fit row justify-center items-center">
       <img :src="source" style="max-width:100%; max-height:100%" />
-    </pinch-zoom>
-  </div>
-  <div v-else class="fit row justify-center items-center">
-    <img :src="source" style="max-width:100%; max-height:100%" />
+    </div>
+    <q-spinner
+      class="absolute-center"
+      v-if="loading"
+      color="white"
+      size="3em"
+    />
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, onMounted } from 'vue'
 import 'pinch-zoom-element/dist/pinch-zoom.js'
 
-export default {
-  props: {
-    source: {
-      type: String,
-      default: ''
-    },
-    interactive: {
-      type: Boolean,
-      default: true
-    }
+// Props
+const props = defineProps({
+  source: {
+    type: String,
+    default: ''
   },
-  emits: ['image-transformed'],
-  methods: {
-    restore () {
-      const controller = document.querySelector('.pinch-zoom-controller')
-      if (controller) {
-        controller.setTransform({
-          scale: 1,
-          x: 0,
-          y: 0,
-          allowChangeEvent: true
-        })
-      }
-    }
-  },
-  mounted () {
-    this.restore()
+  interactive: {
+    type: Boolean,
+    default: true
+  }
+})
+
+// Emit
+const emit = defineEmits(['image-transformed'])
+  
+// Data
+const loading = ref(true)
+
+// Functions
+function restore () {
+  const controller = document.querySelector('.pinch-zoom-controller')
+  if (controller) {
+    controller.setTransform({
+      scale: 1,
+      x: 0,
+      y: 0,
+      allowChangeEvent: true
+    })
   }
 }
+function onLoaded () {
+  loading.value = false
+}
+
+// Hooks
+onMounted(() => {
+  restore()
+})
 </script>
