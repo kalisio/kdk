88c95de1b2a23d81c24279e8c1aadfcda12b9dcf
cnouguier <ch.nouguier@gmail.com>
2023-12-06 11:23:52 +0100

    chore: removed with restriction

--- map/client/mixins/mixin.catalog-panel.js
+++ map/client/mixins/mixin.catalog-panel.js
@@ -3,24 +3,19 @@ export const catalogPanel = {
     panelStyle () {
       const screenHeight = this.$q.screen.height
       this.scrollAreaMaxHeight = screenHeight * 0.75 // 75vh
-      this.scrollAreaMaxWidth = 390
       if (this.$q.screen.lt.sm) {
         this.scrollAreaMaxHeight = screenHeight * 0.6
-        this.scrollAreaMaxWidth = 300
       } else if (this.$q.screen.lt.md) {
         this.scrollAreaMaxHeight = screenHeight * 0.65
-        this.scrollAreaMaxWidth = 330
       } else if (this.$q.screen.lt.lg) {
         this.scrollAreaMaxHeight = screenHeight * 0.7
-        this.scrollAreaMaxWidth = 360
       }
-      return `height: ${this.scrollAreaMaxHeight}px; width: ${this.scrollAreaMaxWidth}px`
+      return `height: ${this.scrollAreaMaxHeight}px;`
     }
   },
   data () {
     return {
       scrollAreaMaxHeight: 0,
-      scrollAreaMaxWidth: 390
     }
   }
 }
