bc3df47fee7deb2ae934a630eab5b3008c207bef
Robin Bourianes <robin.bourianes@kalisio.com>
2023-12-22 09:56:10 +0100

    fix: install age & sops in the ci script

--- .travis.test.sh
+++ .travis.test.sh
@@ -3,10 +3,38 @@
 check_code()
 {
    if [ $1 -eq 1 ]; then
-	  echo "$2 has failed [error: $1]"
-	  exit 1
+      echo "$2 has failed [error: $1]"
+      exit 1
   fi
 }
+
+AGE_VERSION=1.1.1
+SOPS_VERSION=3.8.1
+TMP_PATH="$(mktemp -d -p "${XDG_RUNTIME_DIR:-}" kalisio.XXXXXX)"
+
+install_age() {
+    local DL_PATH="$TMP_PATH/age"
+    mkdir "$DL_PATH" && pushd "$DL_PATH" || exit
+    wget -q https://github.com/FiloSottile/age/releases/download/v${AGE_VERSION}/age-v${AGE_VERSION}-linux-amd64.tar.gz
+    # no checksum ...
+    tar xf age-v${AGE_VERSION}-linux-amd64.tar.gz
+    cp age/age ~/.local/bin
+    popd || exit
+}
+
+install_sops() {
+    local DL_PATH="$TMP_PATH/sops"
+    mkdir "$DL_PATH" && pushd "$DL_PATH" || exit
+    wget -q https://github.com/getsops/sops/releases/download/v${SOPS_VERSION}/sops-v${SOPS_VERSION}.linux.amd64
+    wget -q https://github.com/getsops/sops/releases/download/v${SOPS_VERSION}/sops-v${SOPS_VERSION}.checksums.txt
+    sha256sum --ignore-missing --quiet -c sops-v${SOPS_VERSION}.checksums.txt
+    cp sops-v${SOPS_VERSION}.linux.amd64 ~/.local/bin/sops
+    chmod a+x ~/.local/bin/sops
+    popd || exit
+}
+
+install_age
+install_sops
 
 echo -e "machine github.com\n  login $GITHUB_TOKEN" > ~/.netrc
 
