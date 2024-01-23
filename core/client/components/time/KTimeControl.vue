8d7956b5807aaa1d98a4a85fac80a92886aaae38
melanielassarade <melanie.lassarade@gmail.com>
2024-01-23 10:43:30 +0100

    wip: Refactor KTimeline to simplify the UI #808

--- core/client/components/time/KTimeControl.vue
+++ core/client/components/time/KTimeControl.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="row items-center nowrap">
-    <KAction id="previous-step" icon="" tooltip="KTimeControl.PREVIOUS_STEP" handler="onPreviousStepClicked" />
-    <KAction id="previous-hour" icon="" tooltip="KTimeControl.PREVIOUS_HOUR" handler="onPreviousHourClicked"/>
-    <KAction id="previous-day" icon="" tooltip="KTimeControl.PREVIOUS_DAY" handler="onPreviousDayClicked"/>
-    <KAction id="set-now" icon="" tooltip="KTimeControl.SET_NOW" handler="..."/>
+    <KAction id="previous-step" icon="las la-step-backward" size="md" color="primary" tooltip="KTimeControl.PREVIOUS_STEP" handler="onPreviousStepClicked" />
+    <KAction id="previous-hour" icon="las la-angle-left" size="md" color="primary" tooltip="KTimeControl.PREVIOUS_HOUR" handler="onPreviousHourClicked"/>
+    <KAction id="previous-day" icon="las la-calendar-minus" size="md" color="primary" tooltip="KTimeControl.PREVIOUS_DAY" handler="onPreviousDayClicked"/>
+    <KAction id="timecontrol-now" icon="las la-user-clock" size="md" color="primary" tooltip="KTimeControl.SET_NOW" handler="onNowClicked"/>
     <q-btn-group>
-      <KAction id="set-date" label="..." tooltip="KTimeControl.SET_DATE" handler="..."/>
-      <KAction id="set-time" label="..." tooltip="KTimeCOntrol.SET_TIME" handler="..."/>
+      <KAction id="timecontrol-date" label=".." tooltip="KTimeControl.SET_DATE" handler=".."/>
+      <KAction id="timecontrol-time" label="formatTime(time, 'date.long') + ' ' + formatTime(time, 'time.long')" tooltip="KTimeControl.SET_TIME" handler=".."/>
     </q-btn-group>
-    <q-fab v-model="fabCenter" vertical-actions-align="center" color="primary" label="30" direction="up">
-      <q-fab-action id="" color="primary" @click="onClick" label="05"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="10"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="12"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="15"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="20"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="30"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="60"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="3H"/>
-      <q-fab-action id="" color="primary" @click="onClick" label="6H"/>
+    <q-fab id="timecontrol-step" v-model="fabCenter" vertical-actions-align="center" color="primary" label=".." direction="up">
+      <q-fab-action id="step-05" color="primary" @click="onClick" label="05"/>
+      <q-fab-action id="step-10" color="primary" @click="onClick" label="10"/>
+      <q-fab-action id="step-12" color="primary" @click="onClick" label="12"/>
+      <q-fab-action id="step-15" color="primary" @click="onClick" label="15"/>
+      <q-fab-action id="step-20" color="primary" @click="onClick" label="20"/>
+      <q-fab-action id="step-30" color="primary" @click="onClick" label="30"/>
+      <q-fab-action id="step-60" color="primary" @click="onClick" label="60"/>
+      <q-fab-action id="step-3H" color="primary" @click="onClick" label="3H"/>
+      <q-fab-action id="step-6H" color="primary" @click="onClick" label="6H"/>
     </q-fab>
-    <KAction id="next-day" icon="" tooltip="KTimeControl.NEXT_DAY" handler="onNextDayClicked"/>
-    <KAction id="next-hour" icon="" tooltip="KTimeControl.NEXT_HOUR" handler="onNextHourClicked" />
-    <KAction id="next-step" icon="" tooltip="KTimeControl.NEXT_STEP" handler="onNextStepClicked" />
+    <KAction id="next-day" icon="las la-calendar-plus" size="md" color="primary" tooltip="KTimeControl.NEXT_DAY" handler="onNextDayClicked"/>
+    <KAction id="next-hour" icon="las la-angle-right" size="md" color="primary" tooltip="KTimeControl.NEXT_HOUR" handler="onNextHourClicked" />
+    <KAction id="next-step" icon="las la-step-forward" size="md" color="primary" tooltip="KTimeControl.NEXT_STEP" handler="onNextStepClicked" />
   </div>
 </template>
 <script setup>
-
 </script>
