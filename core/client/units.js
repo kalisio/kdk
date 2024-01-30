563d345999d3635182478b0367e38253f648dc64
Luc Claustres <luc.claustres@orange.fr>
2024-01-30 16:15:30 +0100

    fix: equivalent dose rate definition

--- core/client/units.js
+++ core/client/units.js
@@ -174,22 +174,22 @@ const equivalentDoseRate = {
   'svh': {
     symbol: 'units.SIEVERT_PER_HOUR_SYMBOL',
     label: 'units.SIEVERT_PER_HOUR_LABEL',
-    definition: '3600 svs'
+    definition: '0.000277778 svs'
   },
   'msvh': {
     symbol: 'units.MILLISIEVERT_PER_HOUR_SYMBOL',
     label: 'units.MILLISIEVERT_PER_HOUR_LABEL',
-    definition: '3.600 svs'
+    definition: '0.000000277778 svs'
   },
   'usvh': {
     symbol: 'units.MICROSIEVERT_PER_HOUR_SYMBOL',
     label: 'units.MICROSIEVERT_PER_HOUR_LABEL',
-    definition: '0.0036 svs'
+    definition: '0.000000000277778 svs'
   },
   'nsvh': {
     symbol: 'units.NANOSIEVERT_PER_HOUR_SYMBOL',
     label: 'units.NANOSIEVERT_PER_HOUR_LABEL',
-    definition: '0.0000036 svs'
+    definition: '0.000000000000277778 svs'
   }
 }
 
