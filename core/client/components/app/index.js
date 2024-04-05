8290f6824d9dc97acd7227ada977a69b292ed03e
cnouguier <ch.nouguier@gmail.com>
2024-04-05 19:25:32 +0200

    feat: Remove KTerms component #844

--- core/client/components/app/index.js
+++ core/client/components/app/index.js
@@ -1,11 +1,15 @@
 import KAbout from './KAbout.vue'
 import KHome from './KHome.vue'
 import KPlatform from './KPlatform.vue'
+import KSettings from './KSettings.vue'
+import KTour from './KTour.vue'
 import KWelcome from './KWelcome.vue'
 
 export {
   KAbout,
   KHome,
   KPlatform,
+  KSettings,
+  KTour,
   KWelcome
 }
