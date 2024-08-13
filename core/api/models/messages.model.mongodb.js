77a27b93ad1c734e8f3f4279e6ee26519b77a91f
cnouguier <ch.nouguier@gmail.com>
2024-08-13 15:59:16 +0200

    wip: Provide a messages service #908

--- core/api/models/messages.model.mongodb.js
+++ core/api/models/messages.model.mongodb.js
@@ -4,4 +4,6 @@ export default function (app, options) {
   // Collation provided in query ensure sorting to be case insensitive w.r.t. user's language
   // We built indices with collation to cover the most used languages, it requires different naming...
   options.Model.createIndex({ createdAt: -1 })
+  options.Model.createIndex({ body: 1 }, { name: 'body-en', collation: { locale: 'en', strength: 1 } })
+  options.Model.createIndex({ body: 1 }, { name: 'body-fr', collation: { locale: 'fr', strength: 1 } })
 }
