1ec07645b0f390ce9a5f7d61861e8e1ff340d591
cnouguier <ch.nouguier@gmail.com>
2024-11-20 21:21:48 +0100

    wip: Provide a messages service #908

--- core/client/composables/messages.js
+++ core/client/composables/messages.js
@@ -4,8 +4,8 @@ export function useMessages (contextId) {
   const messagesService = api.getService('messages', contextId)
 
   // Functions
-  async function createMessage (message) {
-    return messagesService.create(message)
+  async function createMessage (message, query) {
+    return messagesService.create(message, { query })
   }
 
   // Expose
