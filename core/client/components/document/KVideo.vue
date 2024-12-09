7ebd9497fab8acdaee7b69b44c00d29073aacffa
cnouguier <ch.nouguier@gmail.com>
2024-12-09 11:54:34 +0100

    feat: Provide a document-based component capable of displaying video content. (close #1011)

---
+++ core/client/components/document/KVideo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="url" class="fit">
+    <video 
+      :src="url" 
+      :controls="controls"
+      :autoplay="autoplay"
+      class="fit" 
+    />
+  </div>
+</template>
+
+<script setup>
+import _ from 'lodash'
+import { computed } from 'vue'
+
+// Props
+const props = defineProps({
+  url: {
+    type: String,
+    default: null
+  },
+  options: {
+    type: Object,
+    default: () => {
+      return {
+        controls: true,
+        autoplay: true
+      }
+    }
+  }
+})
+
+// Computed
+const controls = computed(() => {
+  return _.get(props.options, 'controls', true)
+})
+const autoplay = computed(() => {
+  return _.get(props.options, 'autoplay', true)
+})
+</script>
