216d22801e09b0a7559ff5abe3724e8678d68e18
Luc Claustres <luc.claustres@orange.fr>
2024-12-18 20:15:38 +0100

    feat: Provide an OAuth login screen (closes #1030)

---
+++ core/client/components/screen/KOAuthLoginScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <KScreen
+    :actions="actions"
+  />
+</template>
+
+<script setup>
+import _ from 'lodash'
+import config from 'config'
+import { ref } from 'vue'
+import KScreen from './KScreen.vue'
+
+// Data
+const actions = ref(_.get(config, 'screens.login.actions', []))
+</script>
