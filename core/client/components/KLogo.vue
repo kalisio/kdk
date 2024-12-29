557d1964cf9717bc283f4de0c54bd73e447a8d69
cnouguier <ch.nouguier@gmail.com>
2024-12-29 23:07:09 +0100

    wip: Allow to define the prefix used for keys to store data in localStorage #1019

--- core/client/components/KLogo.vue
+++ core/client/components/KLogo.vue
@@ -15,8 +15,8 @@ import config from 'config'
 import { ref } from 'vue'
 
 // Data
-const appName = _.get(config, 'appName')
-const logo = ref(_.get(config, 'appLogo', _.toLower(appName) + '-logo.png'))
+const appSlug = _.get(config, 'appSlug', _.kebabCase(_.get(config, 'appName')))
+const logo = _.get(config, 'appLogo', `${appSlug}-logo.png`)
 
 // Props
 defineProps({
