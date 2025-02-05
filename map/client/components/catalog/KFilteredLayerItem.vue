6b16f4ce7e64d8d52552560cc2913b9e2e157ea0
cnouguier <ch.nouguier@gmail.com>
2025-02-05 16:21:12 +0100

    feat: Enhance KFilteredLayerItem behavior (close #1080)

--- map/client/components/catalog/KFilteredLayerItem.vue
+++ map/client/components/catalog/KFilteredLayerItem.vue
@@ -1,17 +1,36 @@
 <template>
-  <k-layer-item v-if="!layer.filters" v-bind="$props" :layer="layer"/>
-  <q-tree v-else :nodes="[layer]"
-    dense node-key="label" label-key="label" children-key="filters">
+  <KLayerItem v-if="!layer.filters" 
+    v-bind="$props" 
+    :layer="layer"
+  />
+  <q-tree v-else 
+    :nodes="[layer]"
+    node-key="label" 
+    label-key="label" 
+    children-key="filters"
+    dense
+  >
     <template v-slot:default-header="prop">
       <!-- Layer rendering -->
-      <k-layer-item v-if="prop.node.name" v-bind="$props" :layer="layer" @toggled="onToggled"/>
+      <KLayerItem v-if="prop.node.name" 
+        v-bind="$props" 
+        :layer="layer" 
+        @toggled="onToggled"
+      />
       <!-- Filter rendering -->
       <div v-else class="row items-center q-pl-md q-pr-sm no-wrap">
         <!-- Filter toggle -->
-        <q-toggle v-model="prop.node.isActive" :disable="layer.isDisabled" size="xs" @update:modelValue="onFilterToggled(prop.node)" />
+        <q-toggle 
+          :ref="onToggleRefCreated(prop.node)"
+          v-model="prop.node.isActive" 
+          :color="layer.isVisible ? 'primary' : 'grey-5'"
+          :disable="layer.isDisabled || !layer.isVisible" 
+          size="xs" 
+          @update:modelValue="onFilterToggled(prop.node)"
+        />
         <div :class="{
             'text-primary': layer.isVisible,
-            'text-grey-6': layer.isDisabled
+            'text-grey-6': layer.isDisabled || !layer.isVisible || !prop.node.isActive
           }">
           {{ $tie(prop.node.label) }}
           <q-tooltip
@@ -27,27 +46,46 @@
   </q-tree>
 </template>
 
-<script>
+<script setup>
+import _ from 'lodash'
+import { ref, computed } from 'vue'
 import KLayerItem from './KLayerItem.vue'
 
-export default {
-  name: 'k-filtered-layer-item',
-  components: {
-    KLayerItem
-  },
-  props: {
-    layer: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  methods: {
-    onToggled () {
-      this.$emit('toggled', this.layer)
-    },
-    onFilterToggled (filter) {
-      this.$emit('filter-toggled', this.layer, filter)
+// Props
+const props = defineProps({
+  layer: {
+    type: Object,
+    default: () => {}
+  }
+})
+
+// Emits
+const emit = defineEmits(['toggled', 'filter-toggled'])
+
+// Datas
+const filters = ref([])
+
+// Functions
+function onToggleRefCreated (node) {
+  if (node) {
+    if (!_.find(filters.value, filter => {
+      return node.label === filter.label
+    })) filters.value.push(node)
+  }
+}
+function onToggled () {
+  emit('toggled', props.layer)
+  console.log(props.layer.isVisible)
+  if (props.layer.isVisible) {
+    const areFiltersInactive = _.every(filters.value, { isActive: false })
+    if (areFiltersInactive) {
+      for (const filter of filters.value) filter.isActive = true
     }
   }
+}
+function onFilterToggled (filter) {
+  emit('filter-toggled', props.layer, filter)
+  const areFiltersInactive = _.every(filters.value, { isActive: false })
+  if (areFiltersInactive) emit('toggled', props.layer)
 }
 </script>
