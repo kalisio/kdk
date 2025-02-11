8c277318d3ea6b4261d7af028d252a53ca6ec17e
cnouguier <ch.nouguier@gmail.com>
2025-02-11 12:48:07 +0100

    refactor: migrated KLayersSelector to composition API

--- map/client/components/catalog/KLayersSelector.vue
+++ map/client/components/catalog/KLayersSelector.vue
@@ -13,66 +13,67 @@
       </template>
     </div>
     <div v-else-if="!options.hideIfEmpty" class="row justify-center q-pa-sm">
-      <KStamp icon="las la-exclamation-circle" icon-size="sm" :text="$t('KLayersSelector.NO_LAYER_AVAILABLE')" direction="horizontal" />
+      <KStamp 
+        icon="las la-exclamation-circle" 
+        icon-size="sm" 
+        :text="$t('KLayersSelector.NO_LAYER_AVAILABLE')" 
+        direction="horizontal" 
+      />
     </div>
     <slot name="footer" />
   </div>
 </template>
 
-<script>
+<script setup>
 import _ from 'lodash'
+import { computed } from 'vue'
 import { utils } from '../../../../core/client'
-import { KStamp } from '../../../../core/client/components'
+import { KStamp } from '../../../../core/client/components/KStamp.vue'
 
-export default {
-  name: 'k-layers-selector',
-  components: {
-    KStamp
+// Props
+const props = defineProps({
+  layers: {
+    type: Array,
+    default: () => []
   },
-  props: {
-    layers: {
-      type: Array,
-      default: () => []
-    },
-    options: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  computed: {
-    layerRenderer () {
-      return {
-        component: utils.loadComponent(_.get(this.options, 'renderer', 'catalog/KFilteredLayerItem')),
-        options: _.get(this.options, 'renderer.options', {})
-      }
-    }
-  },
-  methods: {
-    toggleLayer (layer) {
-      const toggleAction = _.find(layer.actions, { id: 'toggle' })
-      if (toggleAction) toggleAction.handler()
-    },
-    async onLayerToggled (layer) {
-      if (layer.isDisabled) return
-      if (this.options.exclusive) {
-        // Due to v-model the visible flag has already been changed on the layer
-        // Simply reset others layers before activating the new one to avoid any problem
-        const visibleLayers = _.filter(this.layers, { isVisible: true })
-        for (let i = 0; i < visibleLayers.length; i++) {
-          const visibleLayer = visibleLayers[i]
-          if (visibleLayer !== layer) await this.toggleLayer(visibleLayers[i])
-        }
-      }
-      await this.toggleLayer(layer)
-    },
-    toggleLayerFilter (layer, filter) {
-      const toggleFilterAction = _.find(layer.actions, { id: 'toggle-filter' })
-      if (toggleFilterAction) toggleFilterAction.handler(filter)
-    },
-    onLayerFilterToggled (layer, filter) {
-      if (layer.isDisabled) return
-      this.toggleLayerFilter(layer, filter)
+  options: {
+    type: Object,
+    default: () => {}
+  }
+})
+
+// Computed
+const layerRenderer = computed(() => {
+  return {
+    component: utils.loadComponent(_.get(props.options, 'renderer', 'catalog/KFilteredLayerItem')),
+    options: _.get(props.options, 'renderer.options', {})
+  }
+})
+
+// Functions
+function  toggleLayer (layer) {
+  const toggleAction = _.find(layer.actions, { id: 'toggle' })
+  if (toggleAction) toggleAction.handler()
+}
+async function onLayerToggled (layer) {
+  if (layer.isDisabled) return
+  if (props.options.exclusive) {
+    // Due to v-model the visible flag has already been changed on the layer
+    // Simply reset others layers before activating the new one to avoid any problem
+    const visibleLayers = _.filter(props.layers, { isVisible: true })
+    for (let i = 0; i < visibleLayers.length; i++) {
+      const visibleLayer = visibleLayers[i]
+      if (visibleLayer !== layer) await toggleLayer(visibleLayers[i])
     }
   }
+  await toggleLayer(layer)
+}
+function toggleLayerFilter (layer, filter) {
+  const toggleFilterAction = _.find(layer.actions, { id: 'toggle-filter' })
+  if (toggleFilterAction) toggleFilterAction.handler(filter)
+}
+function onLayerFilterToggled (layer, filter) {
+  if (layer.isDisabled) return
+  toggleLayerFilter(layer, filter)
 }
 </script>
