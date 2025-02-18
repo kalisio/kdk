03b8bcf48e457538b8b7c8645c0c82e9f7cf0b83
Luc Claustres <luc.claustres@orange.fr>
2025-02-18 09:31:05 +0100

    fix: Hover directive uses mouseover instead of mouseenter event (closes #1112)

--- core/client/directives/v-hover.js
+++ core/client/directives/v-hover.js
@@ -5,18 +5,22 @@ export const vHover = {
   mounted (el, binding) {
     if (Platform.touch) return
     el.__vHoverEnter__ = binding.value.enter || (() => {})
+    el.__vHoverOver__ = binding.value.over || (() => {})
     el.__vHoverLeave__ = binding.value.leave || (() => {})
 
     // Add Event Listeners
-    el.addEventListener('mouseover', el.__vHoverEnter__)
+    el.addEventListener('mouseenter', el.__vHoverEnter__)
+    el.addEventListener('mouseover', el.__vHoverOver__)
     el.addEventListener('mouseleave', el.__vHoverLeave__)
   },
 
   beforeUnmount (el, binding) {
     // Remove Event Listeners
-    el.removeEventListener('mouseover', el.__vHoverEnter__)
+    el.removeEventListener('mouseenter', el.__vHoverEnter__)
+    el.removeEventListener('mouseover', el.__vHoverOver__)
     el.removeEventListener('mouseleave', el.__vHoverLeave__)
     delete el.__vHoverEnter__
+    delete el.__vHoverOver__
     delete el.__vHoverLeave__
   }
 }
