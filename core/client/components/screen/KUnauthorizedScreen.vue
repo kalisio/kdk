550ed4bdd00ba9586f0944aab651cdcc61500457
cnouguier <ch.nouguier@gmail.com>
2025-03-13 10:52:49 +0100

    feat: Provide an Unauthorized screen (close #1145)

---
+++ core/client/components/screen/KUnauthorizedScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <KScreen
+    :title="message"
+    :actions="actions"
+  />
+</template>
+
+<script setup>
+import _ from 'lodash'
+import config from 'config'
+import KScreen from './KScreen.vue'
+
+// Data
+const message = _.get(config, 'screens.notAuthorized.message', 'KUnauthorizedScreen.MESSAGE')
+const actions = _.get(config, 'screens.error.actions', [])
+</script>
