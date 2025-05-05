af869eec4a1957e5c951ec43a9815d82c2951261
cnouguier <ch.nouguier@gmail.com>
2025-05-05 15:06:25 +0200

    feat: Provide a component that allows stacking one icon on top of another (close #1187) docs: updated docs

---
+++ core/client/components/graphics/KIcon.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-icon
+    v-bind="computedIcon"
+    class="k-icon"
+  >
+    <div 
+      v-if="computedOverlay" 
+      class="k-icon-overlay"
+    >
+      <q-icon v-bind="computedOverlay" />
+    </div>
+  </q-icon>
+</template>
+
+<script setup>
+import _ from 'lodash'
+import { computed } from 'vue'
+import { getHtmlColor } from '../../utils'
+
+// Props
+const props = defineProps({
+  icon: {
+    type: [String, Object],
+    default: undefined
+  }
+})
+
+// Data
+const defaultColor = 'grey-9'
+
+// Computed
+const computedIcon = computed(() => {
+  if (!props.icon) return
+  if (_.isString(props.icon)) return { name: props.icon }
+  return _.omit(props.icon, ['overlay', 'color'])
+})
+const computedIconColor = computed(() => {
+  if (!props.icon) return
+  if (_.isString(props.icon)) return defaultColor
+  return getHtmlColor(props.icon.color, defaultColor)
+})
+const computedOverlay = computed(() => {
+  if (!props.icon && !props.icon.overlay) return
+  return _.omit(props.icon.overlay, ['color', 'rotation'])
+})
+const computedOverlayColor = computed(() => {
+  return getHtmlColor(_.get(props.icon.overlay, 'color', defaultColor))
+})
+const computedOverlayTransform = computed(() => {
+  return `rotate(${_.get(props.icon.overlay, 'rotation', 0)}deg)`
+})
+</script>
+
+<style lang="scss" scoped>
+.k-icon {
+  position: relative;
+  color: v-bind(computedIconColor);
+}
+.k-icon-overlay {
+  position: absolute;
+  color: v-bind(computedOverlayColor);
+  transform: v-bind(computedOverlayTransform);
+}
+</style>
---
+++ docs/api/core/components/graphics.md
@@ -0,0 +1,34 @@
+# Graphics
+
+The `graphics` folder provides various components that allow users to display various graphical components.
+
+## `KIcon`
+
+The `KIcon` component is a wrapper around [Quasar Icon](https://quasar.dev/vue-components/icon/) that enables displaying a primary icon with an optional stacked overlay icon. This is useful for creating composite or symbolic icons by layering one icon on top of another.
+
+### Props
+
+| Prop   | Type               | Default     | Description                                                                       |
+| ------ | ------------------ | ----------- | --------------------------------------------------------------------------------- |
+| `icon` | `String \| Object` | `undefined` | Main icon name (as string) or an object describing the main icon and its overlay. |
+
+### Usage
+
+* An icon using a name:
+
+```html
+<KIcon icon='las la-home' />
+```
+* And the same icon with a red slash through it:
+
+``html
+<KIcon
+  :icon="{
+    name: 'las la-home',
+    overlay: {
+      name: 'las la-slash',
+      color: 'red',
+      rotation: 90
+    }
+  }"
+/>
