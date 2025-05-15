c1dd462daeb9dfa43172183a55f14709ff465110
Luc Claustres <luc.claustres@orange.fr>
2025-05-15 16:50:03 +0200

    fix: Unique constraint on catalog model can be a problem in some applications (closes #1208)

--- map/api/models/catalog.model.mongodb.js
+++ map/api/models/catalog.model.mongodb.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export default function (app, options) {
   const db = options.db || app.db
   options.Model = db.collection('catalog')
@@ -9,6 +11,18 @@ export default function (app, options) {
   options.Model.indexExists('name_1_type_1').then(() => options.Model.dropIndex('name_1_type_1')).catch(() => {})
   // Collation provided in query ensure sorting to be case insensitive w.r.t. user's language
   // We built indices with collation to cover the most used languages, it requires different naming...
-  options.Model.createIndex({ name: 1, type: 1 }, { unique: true, name: 'name-type-en', collation: { locale: 'en', strength: 1 } })
-  options.Model.createIndex({ name: 1, type: 1 }, { unique: true, name: 'name-type-fr', collation: { locale: 'fr', strength: 1 } })
+  const languages = ['en', 'fr']
+  options.Model.indexInformation({ full: true }).then(indexes => {
+    for (let i = 0; i < languages.length; i++) {
+      const language = languages[i]
+      const name = `name-type-${language}`
+      const createIndex = () => {
+        return options.Model.createIndex({ name: 1, type: 1 }, { name, collation: { locale: language, strength: 1 } })
+      }
+      // We also previously had a unique constraint that we have relaxed
+      let uniqueIndex = _.find(indexes, { name, unique: true })
+      if (uniqueIndex) options.Model.dropIndex(name).then(createIndex).catch(() => {})
+      else createIndex()
+    }
+  })
 }
