a6ddafd2f3dd81c9da9d952a11618a24c1464bec
cnouguier <ch.nouguier@gmail.com>
2025-06-06 21:56:04 +0200

    fix: Fullscreen action loses its state when displayed in a menu (close #1240)

--- core/client/components/action/KToggleFullscreenAction.vue
+++ core/client/components/action/KToggleFullscreenAction.vue
@@ -1,25 +1,16 @@
 <template>
   <KAction
-    ref="actionRef"
     v-bind="props"
+    :toggled="Fullscreen"
     :handler="toggleFullscreen"
   />
 </template>
 
 <script setup>
 import _ from 'lodash'
-import { ref, watch } from 'vue'
 import { actionProps, Fullscreen, toggleFullscreen } from '../../utils'
 import KAction from './KAction.vue'
 
 // Props
-const props = defineProps(_.omit(actionProps, ['url', 'handler', 'dialog', 'route', 'closePopup']))
-
-// Data
-const actionRef = ref(null)
-
-// Watch
-watch(Fullscreen, () => {
-  if (actionRef.value.isToggled !== Fullscreen.value) actionRef.value.toggle()
-})
+const props = defineProps(_.omit(actionProps, ['url', 'handler', 'dialog', 'route', 'closePopup', 'toggled']))
 </script>
