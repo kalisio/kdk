df2d6ae6e58ba240afddc69708bfb7e0789df1c5
kalisio <kalisio@knuc3.home>
2025-06-13 12:10:52 +0200

    test: update push test

--- test/api/core/push.test.js
+++ test/api/core/push.test.js
@@ -175,7 +175,6 @@ describe('core:push', () => {
     expect(operation.succesful.length === 1).beTrue()
     expect(operation.succesful[0].statusCode).to.equal(201)
     expect(operation.failed.length === 1).beTrue()
-    expect(operation.failed[0].statusCode).to.equal(404)
     expect(operation.failed[0].endpoint).to.equal(expiredSubscription.endpoint)
   })
   // Let enough time to process
