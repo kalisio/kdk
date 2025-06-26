f48caacdbcea7f6c693cfb96c8a7b0ab1f39ddea
cnouguier <ch.nouguier@gmail.com>
2025-06-26 08:17:48 +0200

    chore: fixed typo

--- core/common/utils.js
+++ core/common/utils.js
@@ -23,7 +23,7 @@ export function buildEncodedUrl (baseUrl, parameters) {
   return encodeURI(buildEncodedUrl(baseUrl, parameters))
 }
 
-// Remove diac
+// Remove diacritics
 export function makeDiacriticPattern (pattern, options = {}) {
   // List of families of diacritics, the first character in a family is the one without a diacritic
   const diacritics = ['a,á,à,ä,â,ã', 'e,é,ë,è,ê', 'i,í,ï,ì,î', 'o,ó,ö,ò,õ,ô', 'u,ü,ú,ù,û', 'c,ç']
@@ -32,11 +32,11 @@ export function makeDiacriticPattern (pattern, options = {}) {
   for (const character of pattern) {
     // Iterate over all diacritics to find matching one if any
     const family = diacritics.find(family => {
-      // The reverse option is used to allow for any diacristic or
-      // the character without a diacristic to be matched by any other one
+      // The reverse option is used to allow for any diacritic or
+      // the character without a diacritic to be matched by any other one
       if (options.reverse) return family.includes(character)
-      // Otherwise by default we allow to match all diacristics
-      // for the character without a diacristic but not the other way around
+      // Otherwise by default we allow to match all diacritics
+      // for the character without a diacritic but not the other way around
       else return family[0] === character
     })
     // If not a diacritic simply leave as it
