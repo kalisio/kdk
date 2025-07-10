c70d764b7e499a8243ba0f191269c74f73b8f9a9
Floran05 <carvalhofloran@gmail.com>
2025-07-10 16:39:35 +0200

    wip: Create a KItemsFilter component (#1255)

---
+++ core/client/components/collection/KItemsFilter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-xs">
+    <q-input
+      :label="$tie(label)"
+      v-model="inputValue"
+      clearable
+      borderless
+      dense
+      debounce="500"
+      @update:model-value="onSearch"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// Props
+const props = defineProps({
+  value: {
+    type: String,
+    default: ''
+  },
+  label: {
+    type: String,
+    default: 'KFilter.SEARCH_LABEL'
+  },
+  onSearch: {
+    type: Function,
+    default: () => {}
+  }
+})
+
+// Data
+const inputValue = ref(props.value)
+
+</script>
