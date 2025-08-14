3db66a4d457e0c337ec8d8d05035afa53f0b766f
Floran05 <carvalhofloran@gmail.com>
2025-08-14 14:16:39 +0200

    wip: Create a KTagManager component (#1254)

--- core/client/components/tags/KTagManager.vue
+++ core/client/components/tags/KTagManager.vue
@@ -15,43 +15,7 @@
       />
     </div>
     <KFollower
-      :follower="{
-        component: 'layout/KFab',
-        direction: 'up',
-        alignment: 'right',
-        content: [{
-          id: 'create-tag',
-          icon: 'las la-plus',
-          tooltip: 'KTagManager.CREATE_TAG',
-          dialog: {
-            title: 'KTagManager.CREATE_TAG',
-            component: 'KEditor',
-            service: 'tags',
-            object: ':item',
-            hideButtons: true,
-            baseObject: getBaseObject(),
-            beforeRequest: (values) => {
-              // If more than one service is available, we need to set the property in which the tags will be stored
-              if (!_.isEmpty(props.services) && props.services.length > 1) {
-                _.forEach(props.services, (service) => {
-                  if (service.name === values.service) {
-                    values.property = service.property
-                  }
-                })
-                if (!_.get(values, 'property')) return { isOk: false }
-              }
-              return { isOk: true, values }
-            },
-            schema: schema,
-            cancelAction: 'CANCEL',
-            okAction: {
-              id: 'ok-button',
-              label: 'CREATE',
-              handler: 'apply'
-            }
-          }
-        }]
-      }"
+      :follower="follower"
       targetId="left-window-magnet"
       anchor="bottom-right"
     />
@@ -60,7 +24,7 @@
 
 <script setup>
 import _ from 'lodash'
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, watch, onMounted } from 'vue'
 import { KGrid } from '..'
 import KFollower from '../KFollower.vue'
 
@@ -76,6 +40,43 @@ const props = defineProps({
 const baseQuery = ref({ $sort: { name: 1 } })
 const filterQuery = ref({})
 const baseSchema = ref(null)
+const follower = ref({
+  component: 'layout/KFab',
+  direction: 'up',
+  alignment: 'right',
+  content: [{
+    id: 'create-tag',
+    icon: 'las la-plus',
+    tooltip: 'KTagManager.CREATE_TAG',
+    dialog: {
+      title: 'KTagManager.CREATE_TAG',
+      component: 'KEditor',
+      service: 'tags',
+      object: ':item',
+      hideButtons: true,
+      baseObject: getBaseObject(),
+      beforeRequest: (values) => {
+        // If more than one service is available, we need to set the property in which the tags will be stored
+        if (!_.isEmpty(props.services) && props.services.length > 1) {
+          _.forEach(props.services, (service) => {
+            if (service.name === values.service) {
+              values.property = service.property
+            }
+          })
+          if (!_.get(values, 'property')) return { isOk: false }
+        }
+        return { isOk: true, values }
+      },
+      schema: {},
+      cancelAction: 'CANCEL',
+      okAction: {
+        id: 'ok-button',
+        label: 'CREATE',
+        handler: 'apply'
+      }
+    }
+  }]
+})
 
 // Computed
 const toolbar = computed(() => {
@@ -83,7 +84,7 @@ const toolbar = computed(() => {
     {
       id: 'tags-filter',
       component: 'collection/KItemsFilter',
-      class: 'col q-ml-md',
+      class: 'col q-ml-sm',
       onSearch: (value) => {
         if (_.isEmpty(value)) {
           filterQuery.value = {}
@@ -95,7 +96,7 @@ const toolbar = computed(() => {
     {
       component: 'collection/KItemsSorter',
       id: 'tags-sorter',
-      class: 'q-mr-md',
+      class: 'q-mr-sm',
       tooltip: 'KTagManager.SORT_TAGS',
       options: [
         { icon: 'las la-sort-alpha-down', value: { field: 'name', order: 1 }, default: true },
@@ -171,6 +172,11 @@ const viewRenderer = computed(() => {
   }
 })
 
+// Watch
+watch(schema, (newSchema) => {
+  follower.value.content[0].dialog.schema = newSchema
+})
+
 // Functions
 function hasOneService () {
   return !_.isEmpty(props.services) && props.services.length === 1
@@ -185,7 +191,7 @@ function getBaseObject () {
 }
 
 onMounted(async () => {
-  const schema = await import(`@schemas/tags.update.json`)
+  const schema = await import('@schemas/tags.update.json')
   baseSchema.value = schema.default
 })
 
