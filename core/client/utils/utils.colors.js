f6e35debc53f006371ad2b5b96664359fbc36796
cnouguier <ch.nouguier@gmail.com>
2025-10-22 15:48:40 +0200

    wip: Chip color rendering is not properly handled on iOS #1329

--- core/client/utils/utils.colors.js
+++ core/client/utils/utils.colors.js
@@ -3,12 +3,6 @@ import logger from 'loglevel'
 import { colors } from 'quasar'
 import chroma from 'chroma-js'
 
-export const QuasarFamilies = [
-  'red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue',
-  'cyan', 'teal', 'green', 'light-green', 'lime', 'yellow', 'amber',
-  'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'
-]
-
 export const QuasarPalette = {
   red: '#f44336',
   'red-1': '#ffebee',
