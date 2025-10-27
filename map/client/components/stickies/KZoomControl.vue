09df4ac65a41094f4299476fa01a4a237069428a
Arthur DURAND <arthur.durand@kalisio.com>
2025-10-27 13:10:37 +0100

    wip: Provide a zoom control sticky #1332

---
+++ map/client/components/stickies/KZoomControl.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="'k-zoom-control ' + (props.vertical ? 'column reverse' : 'row')">
+    <KAction
+      id="zoom-out"
+      icon="remove"
+      tooltip="mixins.activity.ZOOM_OUT"
+      renderer="button"
+      :handler="onZoomOut"
+    />
+    <q-separator :vertical="!props.vertical" />
+    <KAction
+      id="zoom-in"
+      icon="add"
+      tooltip="mixins.activity.ZOOM_IN"
+      renderer="button"
+      :handler="onZoomIn"
+    />
+  </div>
+</template>
+
+<script setup>
+import { composables as kCoreComposables } from '@kalisio/kdk/core.client'
+
+const { CurrentActivity } = kCoreComposables.useCurrentActivity()
+const { onZoomOut, onZoomIn } = CurrentActivity.value
+
+const props = defineProps({
+  vertical: {
+    default: false,
+    type: Boolean
+  }
+})
+</script>
+
+<style>
+.k-zoom-control {
+  border: 1px solid lightgrey;
+  background: white;
+}
+</style>
