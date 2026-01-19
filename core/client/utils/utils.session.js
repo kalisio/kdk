d5f4a13d015430fd7f019617b40f1d66cd132a09
Luc Claustres <luc.claustres@orange.fr>
2026-01-19 17:43:59 +0100

    fix: Harden local cache access (closes #1387)

--- core/client/utils/utils.session.js
+++ core/client/utils/utils.session.js
@@ -12,7 +12,12 @@ async function authenticate(authentication) {
   let user = Store.get('user')
   if (user) return
   // Store latest authentication data for offline mode
-  await LocalCache.setItem('authentication', authentication)
+  // Avoid blocking on eg QuotaExceededError
+  try {    
+    await LocalCache.setItem('authentication', authentication)
+  } catch (error) {
+    logger.error(error)
+  }
   // We must be sure to do this before setting the user as it usually triggers
   // applications to display components requiring an authenticated user.
   // Do it based on the 'login' event does not work fine due to interleaved async operations
