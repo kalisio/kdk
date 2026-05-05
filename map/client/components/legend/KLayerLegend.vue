2114c9efacb41d61718dcb5d54099c2fc2434dd3
Luc Claustres <luc.claustres@orange.fr>
2026-05-05 19:53:41 +0200

    wip: Provide an option to hide a layer legend if no features are currently visible in map view (#1464)

--- map/client/components/legend/KLayerLegend.vue
+++ map/client/components/legend/KLayerLegend.vue
@@ -1,7 +1,7 @@
 <template>
   <template v-for="legend in legends" :key="legend.label">
     <div class="q-py-xs q-px-md">
-      <component
+      <component 
         :is="legend.renderer"
         :layer="layer"
         v-bind="legend.props"
@@ -13,7 +13,12 @@
 <script setup>
 import _ from 'lodash'
 import logger from 'loglevel'
-import { computed } from 'vue'
+import sift from 'sift'
+import { ref, reactive, computed, watch } from 'vue'
+import bboxPolygon from '@turf/bbox-polygon'
+import intersects from '@turf/boolean-intersects'
+import { featureEach } from '@turf/meta'
+import { useCurrentActivity } from '../../composables'
 import { utils as coreUtils } from '../../../../core/client'
 
 // Props
@@ -36,16 +41,24 @@ const props = defineProps({
   }
 })
 
+// Data
+const { CurrentActivity } = useCurrentActivity({ selection: false, probe: false })
+const layerHasVisibleFeatures = ref(false)
+const filterHasVisibleFeatures = reactive({})
+const requestRefresh = _.debounce(refresh, 300)
+
 // Computed
 const legends = computed(() => {
   const result = []
+  const hideWhenNoFeaturesVisible = _.get(props.layer, 'hideLegendWhenNoFeaturesVisible', true)
+  if (hideWhenNoFeaturesVisible && !layerHasVisibleFeatures.value) return result
   let layerLegends = props.layer.legend || []
   if (!Array.isArray(layerLegends)) layerLegends = [layerLegends]
   // Check if layer has filters with own legend
   if (Array.isArray(props.layer.filters)) {
     props.layer.filters.forEach((filter) => {
-      // Include when filter is active and has a legend
-      if (!filter.isActive || !filter.legend) { return }
+      // Include when filter is active, has a legend and has feature
+      if (!filter.isActive || !filter.legend || (hideWhenNoFeaturesVisible && !filterHasVisibleFeatures[filter.label])) return
       const filterLegends = Array.isArray(filter.legend) ? filter.legend : [filter.legend]
       layerLegends.push(...filterLegends)
     })
@@ -74,4 +87,56 @@ const legends = computed(() => {
   })
   return result
 })
+
+// Watch
+watch(CurrentActivity, (newActivity, oldActivity) => {
+  if (oldActivity) {
+    oldActivity.value.$engineEvents.off('moveend', requestRefresh)
+  }
+  if (newActivity) {
+    newActivity.$engineEvents.on('moveend', requestRefresh)
+  }
+}, { immediate: true })
+
+// Function
+function refresh() {
+  if (!CurrentActivity.value) return
+  // Check if any feature of the layer is visible in current map bounds
+  const [[south, west], [north, east]] = CurrentActivity.value.getBounds()
+  const bounds = bboxPolygon([west, south, east, north])
+  const geoJson = CurrentActivity.value.toGeoJson(props.layer.name)
+  layerHasVisibleFeatures.value = false
+  // Check if layer has filters with own legend
+  if (Array.isArray(props.layer.filters)) {
+    props.layer.filters.forEach((filter) => {
+      // Include when filter is active and has a legend
+      if (!filter.isActive || !filter.legend) { return }
+      filterHasVisibleFeatures[filter.label] = false
+    })
+  }
+  featureEach(geoJson, (feature) => {
+    const isFeatureInBounds = intersects(feature, bounds)
+    // If at least one feature found for layer no need to test more
+    if (!layerHasVisibleFeatures.value && isFeatureInBounds) {
+      layerHasVisibleFeatures.value = true
+    }
+    // Check if feature match any filter as well
+    if (Array.isArray(props.layer.filters)) {
+      props.layer.filters.forEach((filter) => {
+        // Include when filter is active and has a legend
+        if (!filter.isActive || !filter.legend) return
+        // If at least one feature found for filter no need to test more
+        if (!filterHasVisibleFeatures[filter.label] && isFeatureInBounds) {
+          const filteredFeatures = [feature].filter(sift(filter.active))
+          if (!_.isEmpty(filteredFeatures)) {
+            filterHasVisibleFeatures[filter.label] = true
+          }
+        }
+      })
+    }
+  })
+}
+
+// Immediate
+refresh()
 </script>
